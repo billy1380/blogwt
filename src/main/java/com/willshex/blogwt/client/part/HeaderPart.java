86a4d1693bd9000cf698062205555e6f964ec6be
billy1380 <william.shakour@gmail.com>
2015-05-16 18:14:12 +0100

    sorted sign in/out process

--- src/main/java/com/willshex/blogwt/client/part/HeaderPart.java
+++ src/main/java/com/willshex/blogwt/client/part/HeaderPart.java
@@ -7,22 +7,47 @@
 //
 package com.willshex.blogwt.client.part;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.DivElement;
+import com.google.gwt.dom.client.ImageElement;
+import com.google.gwt.dom.client.SpanElement;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.InlineHyperlink;
 import com.google.gwt.user.client.ui.Widget;
+import com.willshex.blogwt.client.DefaultEventBus;
 import com.willshex.blogwt.client.Resources;
 import com.willshex.blogwt.client.controller.PropertyController;
+import com.willshex.blogwt.client.controller.SessionController;
+import com.willshex.blogwt.client.page.PageType;
+import com.willshex.blogwt.shared.api.datatype.Session;
+import com.willshex.blogwt.shared.api.datatype.User;
+import com.willshex.blogwt.shared.api.user.call.LoginRequest;
+import com.willshex.blogwt.shared.api.user.call.LoginResponse;
+import com.willshex.blogwt.shared.api.user.call.LogoutRequest;
+import com.willshex.blogwt.shared.api.user.call.LogoutResponse;
+import com.willshex.blogwt.shared.api.user.call.event.LoginEventHandler;
+import com.willshex.blogwt.shared.api.user.call.event.LogoutEventHandler;
+import com.willshex.gson.json.service.shared.StatusType;
 
-public class HeaderPart extends Composite {
+public class HeaderPart extends Composite implements LoginEventHandler,
+		LogoutEventHandler {
 
-	@UiField InlineHyperlink btnTests;
+	private List<HandlerRegistration> registration;
+	
 	@UiField DivElement divName;
+	@UiField InlineHyperlink btnAccount;
+	@UiField InlineHyperlink btnSignInOut;
+	@UiField SpanElement spnUserName;
+
+	@UiField ImageElement imgAvatar;
 
 	private static HeaderPartUiBinder uiBinder = GWT
 			.create(HeaderPartUiBinder.class);
@@ -39,5 +64,75 @@ public class HeaderPart extends Composite {
 
 	@UiHandler("btnExpand")
 	void onBtnExpandClicked (ClickEvent event) {}
+
+	@Override
+	protected void onAttach () {
+		super.onAttach();
+
+		if (registration == null) {
+			registration = new ArrayList<HandlerRegistration>();
+		}
+
+		registration.add(DefaultEventBus.get().addHandlerToSource(
+				LoginEventHandler.TYPE, SessionController.get(), this));
+		registration.add(DefaultEventBus.get().addHandlerToSource(
+				LogoutEventHandler.TYPE, SessionController.get(), this));
+
+		Session session = SessionController.get().session();
+		if (session != null && session.user != null) {
+			setLoggedInUser(session.user);
+		}
+	}
+
+	@Override
+	protected void onDetach () {
+		for (HandlerRegistration handlerRegistration : registration) {
+			handlerRegistration.removeHandler();
+		}
+
+		super.onDetach();
+	}
+
+	/**
+	 * @param user
+	 */
+	private void setLoggedInUser (User user) {
+		imgAvatar.setAlt(user.username);
+		imgAvatar.setSrc(user.avatar + "?s=20&default=retro");
+		spnUserName.setInnerText(user.forename + " " + user.surname);
+		btnAccount.setVisible(true);
+		btnSignInOut.setText("Sign Out");
+		btnSignInOut.setTargetHistoryToken(PageType.LogoutPageType
+				.asTargetHistoryToken());
+	}
+
+	@Override
+	public void logoutSuccess (LogoutRequest input, LogoutResponse output) {
+		btnAccount.setVisible(false);
+		btnSignInOut.setText("Sign In");
+		btnSignInOut.setTargetHistoryToken(PageType.LoginPageType
+				.asTargetHistoryToken());
+	}
+
+	@Override
+	public void logoutFailure (LogoutRequest input, Throwable caught) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void loginSuccess (LoginRequest input, LoginResponse output) {
+		if (output.status == StatusType.StatusTypeSuccess
+				&& output.session != null && output.session.user != null) {
+			setLoggedInUser(output.session.user);
+		}
+
+	}
+
+	@Override
+	public void loginFailure (LoginRequest input, Throwable caught) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
