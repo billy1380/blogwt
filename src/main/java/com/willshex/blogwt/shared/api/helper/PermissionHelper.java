f35648e61f7d2a420f7947419a811f1d86133be8
billy1380 <william.shakour@gmail.com>
2015-05-18 16:22:08 +0100

    add roles and permissions on setup

--- src/main/java/com/willshex/blogwt/shared/api/helper/PermissionHelper.java
+++ src/main/java/com/willshex/blogwt/shared/api/helper/PermissionHelper.java
@@ -7,6 +7,15 @@
 //
 package com.willshex.blogwt.shared.api.helper;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.willshex.blogwt.shared.api.datatype.Permission;
+import com.willshex.blogwt.shared.api.datatype.PermissionTypeType;
+
 /**
  * @author William Shakour (billy1380)
  *
@@ -17,5 +26,51 @@ public class PermissionHelper {
 	public static final String MANAGE_ROLES = "MRO";
 	public static final String MANAGE_USERS = "MUS";
 	public static final String MANAGE_POSTS = "MPO";
+
+	public static final String MANAGE_PERMISSIONS_NAME = "Manage Permissions";
+	public static final String MANAGE_ROLES_NAME = "Manage Roles";
+	public static final String MANAGE_USERS_NAME = "Manage Users";
+	public static final String MANAGE_POSTS_NAME = "Manage Posts";
+
+	public static final String MANAGE_PERMISSIONS_DESCRIPTION = "allows users to add permissions to users";
+	public static final String MANAGE_ROLES_DESCRIPTION = "allows users to add roles to other users as well as add permissions to roles";
+	public static final String MANAGE_USERS_DESCRIPTION = "allows users to add and remove users";
+	public static final String MANAGE_POSTS_DESCRIPTION = "allows users to add and remove posts";
+
+	public static Map<String, Permission> toLookup (
+			Collection<Permission> permissions) {
+		Map<String, Permission> lookup = new HashMap<String, Permission>();
+
+		for (Permission permission : permissions) {
+			lookup.put(permission.code, permission);
+		}
+
+		return lookup;
+	}
+
+	public static Permission createFull (String code, String name,
+			String description) {
+		return new Permission().code(code).name(name).description(description)
+				.type(PermissionTypeType.PermissionTypeTypeUser);
+	}
+
+	public static Permission create (String code) {
+		return new Permission().code(code);
+	}
+
+	public static Collection<Permission> createAll () {
+		List<Permission> all = new ArrayList<Permission>();
+
+		all.add(createFull(MANAGE_PERMISSIONS, MANAGE_PERMISSIONS_NAME,
+				MANAGE_PERMISSIONS_DESCRIPTION));
+		all.add(createFull(MANAGE_ROLES, MANAGE_ROLES_NAME,
+				MANAGE_ROLES_DESCRIPTION));
+		all.add(createFull(MANAGE_USERS, MANAGE_USERS_NAME,
+				MANAGE_USERS_DESCRIPTION));
+		all.add(createFull(MANAGE_POSTS, MANAGE_POSTS_NAME,
+				MANAGE_POSTS_DESCRIPTION));
+
+		return all;
+	}
 
 }
--- src/main/java/com/willshex/blogwt/shared/api/helper/RoleHelper.java
+++ src/main/java/com/willshex/blogwt/shared/api/helper/RoleHelper.java
@@ -7,6 +7,10 @@
 //
 package com.willshex.blogwt.shared.api.helper;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.willshex.blogwt.shared.api.datatype.Role;
 
 /**
@@ -15,8 +19,28 @@ import com.willshex.blogwt.shared.api.datatype.Role;
  */
 public class RoleHelper {
 	public static final String ADMIN = "ADM";
+	public static final String ADMIN_NAME = "System Administrator";
+	public static final String ADMIN_DESCRIPTION = "allows users to perform any action (implicitly contains all permissions)";
 
 	public static Role createAdmin () {
-		return new Role().code(ADMIN);
+		return create(ADMIN);
+	}
+
+	public static Map<String, Role> toLookup (Collection<Role> roles) {
+		Map<String, Role> lookup = new HashMap<String, Role>();
+
+		for (Role role : roles) {
+			lookup.put(role.code, role);
+		}
+
+		return lookup;
+	}
+
+	public static Role createFull (String code, String name, String description) {
+		return new Role().code(code).name(name).description(description);
+	}
+
+	public static Role create (String code) {
+		return new Role().code(code);
 	}
 }
