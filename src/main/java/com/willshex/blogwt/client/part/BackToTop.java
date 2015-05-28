a098431285191ee8f785366a9b57fc7a4931d8d3
billy1380 <william.shakour@gmail.com>
2015-05-28 12:31:57 +0100

    - added gwt query for effects - added back to top button to details page - beautified back to top button with fade and less intense call frequency - added window smooth scroll

--- src/main/java/com/willshex/blogwt/client/part/BackToTop.java
+++ src/main/java/com/willshex/blogwt/client/part/BackToTop.java
@@ -7,18 +7,25 @@
 //
 package com.willshex.blogwt.client.part;
 
+import static com.google.gwt.query.client.GQuery.$;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.query.client.Function;
+import com.google.gwt.query.client.plugins.effects.PropertiesAnimation.EasingCurve;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.Window.ScrollEvent;
 import com.google.gwt.user.client.Window.ScrollHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.willshex.blogwt.client.animation.ScrollWindow;
 
 /**
  * @author William Shakour (billy1380)
@@ -32,6 +39,26 @@ public class BackToTop extends Composite {
 	interface BackToTopUiBinder extends UiBinder<Widget, BackToTop> {}
 
 	@UiField Button btnBackToTop;
+	private HandlerRegistration registration;
+	private Timer toggleTop = new Timer() {
+
+		@Override
+		public void run () {
+			if (Window.getScrollTop() > 200) {
+				$(btnBackToTop).fadeIn(300, new Function() {
+					public void f () {
+						btnBackToTop.getElement().getStyle().setOpacity(1.0);
+					}
+				});
+			} else {
+				$(btnBackToTop).fadeOut(300, new Function() {
+					public void f () {
+						btnBackToTop.getElement().getStyle().setOpacity(0.0);
+					}
+				});
+			}
+		}
+	};
 
 	public BackToTop () {
 		initWidget(uiBinder.createAndBindUi(this));
@@ -44,11 +71,13 @@ public class BackToTop extends Composite {
 										"<span class=\"glyphicon glyphicon-arrow-up aria-hidden=\"true\"></span> ")
 								.asString()
 								+ btnBackToTop.getText());
+
+		btnBackToTop.getElement().getStyle().setOpacity(0.0);
 	}
 
 	@UiHandler("btnBackToTop")
 	void onBtnBackToTopClicked (ClickEvent e) {
-		Window.scrollTo(0, 0);
+		new ScrollWindow(0, EasingCurve.easeInOutQuad).run(300);
 	}
 
 	/* (non-Javadoc)
@@ -58,12 +87,27 @@ public class BackToTop extends Composite {
 	protected void onAttach () {
 		super.onAttach();
 
-		Window.addWindowScrollHandler(new ScrollHandler() {
+		registration = Window.addWindowScrollHandler(new ScrollHandler() {
 
 			@Override
-			public void onWindowScroll (ScrollEvent event) {
-				btnBackToTop.setVisible(event.getScrollTop() > 200);
+			public void onWindowScroll(ScrollEvent event) {
+				toggleTop.cancel();
+				toggleTop.schedule(150);
 			}
 		});
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.google.gwt.user.client.ui.Composite#onDetach()
+	 */
+	@Override
+	protected void onDetach () {
+		super.onDetach();
+		
+		if (registration != null) {
+			registration.removeHandler();
+		}
+		
+		toggleTop.cancel();
 	}
 }
