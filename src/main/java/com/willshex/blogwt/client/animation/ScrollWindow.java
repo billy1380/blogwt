756810c1dce49417cd6ae893bcb1bf10dc780cf2
billy1380 <william.shakour@gmail.com>
2015-05-28 12:31:57 +0100

    - added gwt query for effects - added back to top button to details page - beautified back to top button with fade and less intense call frequency - added window smooth scroll

---
+++ src/main/java/com/willshex/blogwt/client/animation/ScrollWindow.java
@@ -0,0 +1,46 @@
+//
+//  ScrollWindow.java
+//  blogwt
+//
+//  Created by William Shakour (billy1380) on 28 May 2015.
+//  Copyright © 2015 SPACEHOPPER STUDIOS Ltd. All rights reserved.
+//
+package com.willshex.blogwt.client.animation;
+
+import com.google.gwt.animation.client.Animation;
+import com.google.gwt.query.client.plugins.effects.PropertiesAnimation.Easing;
+import com.google.gwt.user.client.Window;
+
+/**
+ * @author William Shakour (billy1380)
+ *
+ */
+public class ScrollWindow extends Animation {
+	private Easing easing;
+	private double startLeft;
+	private double distanceLeft;
+	private double startTop;
+	private double distanceTop;
+
+	public ScrollWindow (int left, int top, Easing easing) {
+		this.easing = easing;
+		startLeft = Window.getScrollLeft();
+		startTop = Window.getScrollTop();
+		this.distanceLeft = left - startLeft;
+		this.distanceTop = top - startTop;
+	}
+
+	public ScrollWindow (int top, Easing easing) {
+		this(Window.getScrollLeft(), top, easing);
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see com.google.gwt.animation.client.Animation#onUpdate(double) */
+	@Override
+	protected void onUpdate (double progress) {
+		Window.scrollTo(
+				(int) (startLeft + (easing.interpolate(progress) * distanceLeft)),
+				(int) (startTop + (easing.interpolate(progress) * distanceTop)));
+	}
+}
