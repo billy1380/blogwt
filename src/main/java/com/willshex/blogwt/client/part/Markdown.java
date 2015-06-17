5aa33aa9816a0306cf909dd1bb6825aad6e78025
billy1380 <william.shakour@gmail.com>
2015-06-17 11:12:26 +0100

    fixed file comments

--- src/main/java/com/willshex/blogwt/client/part/Markdown.java
+++ src/main/java/com/willshex/blogwt/client/part/Markdown.java
@@ -3,7 +3,7 @@
 //  blogwt
 //
 //  Created by William Shakour (billy1380) on 16 Jun 2015.
-//  Copyright © 2015 SPACEHOPPER STUDIOS Ltd. All rights reserved.
+//  Copyright © 2015 WillShex Limited. All rights reserved.
 //
 package com.willshex.blogwt.client.part;
 
