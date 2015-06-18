822534a37d49e2d961b429465dfc9ed31551b6f2
billy1380 <william.shakour@gmail.com>
2015-06-18 12:54:21 +0100

    - more of the toolbar buttons are now functional - removed buttons that are not available via extensions

--- src/main/java/com/willshex/blogwt/client/part/MarkdownToolbar.java
+++ src/main/java/com/willshex/blogwt/client/part/MarkdownToolbar.java
@@ -12,6 +12,7 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.PushButton;
 import com.google.gwt.user.client.ui.TextBoxBase;
@@ -33,14 +34,10 @@ public class MarkdownToolbar extends Composite {
 	@UiField PushButton btnHeading;
 	@UiField PushButton btnBold;
 	@UiField PushButton btnItalic;
-	@UiField PushButton btnUnderline;
-	@UiField PushButton btnSubscript;
 	@UiField PushButton btnSuperscript;
 	@UiField PushButton btnStrikethrough;
 	@UiField PushButton btnFixed;
 
-	@UiField PushButton btnIndent;
-	@UiField PushButton btnOutdent;
 	@UiField PushButton btnQuote;
 	@UiField PushButton btnCode;
 
@@ -62,20 +59,12 @@ public class MarkdownToolbar extends Composite {
 				"<span class=\"glyphicon glyphicon-bold\"></span>");
 		btnItalic.getElement().setInnerHTML(
 				"<span class=\"glyphicon glyphicon-italic\"></span>");
-		btnUnderline.getElement().setInnerHTML(
-				"<span class=\"icon-underline\"></span> underline");
 		btnStrikethrough.getElement().setInnerHTML(
 				"<span class=\"\"></span> strike");
 		btnSuperscript.getElement().setInnerHTML(
 				"<span class=\"glyphicon glyphicon-superscript\"></span>");
-		btnSubscript.getElement().setInnerHTML(
-				"<span class=\"glyphicon glyphicon-subscript\"></span>");
 		btnFixed.getElement().setInnerHTML("<span class=\"\">fixed</span>");
 
-		btnIndent.getElement().setInnerHTML(
-				"<span class=\"glyphicon glyphicon-indent-left\"></span>");
-		btnOutdent.getElement().setInnerHTML(
-				"<span class=\"glyphicon glyphicon-indent-right\"></span>");
 		btnQuote.getElement().setInnerHTML("<span class=\"\"></span> quote");
 		btnCode.getElement().setInnerHTML(
 				"<span class=\"glyphicon glyphicon-console\"></span>");
@@ -98,17 +87,18 @@ public class MarkdownToolbar extends Composite {
 
 	}
 
-	@UiHandler({ "btnHeading", "btnBold", "btnItalic", "btnUnderline",
-			"btnStrikethrough", "btnSuperscript", "btnSubscript", "btnFixed",
-			"btnIndent", "btnOutdent", "btnQuote", "btnCode", "btnHr", "btnOl",
-			"btnUl", "btnInsertImage", "btnInsertLink", "btnRemoveFormat" })
+	@UiHandler({ "btnHeading", "btnBold", "btnItalic", "btnStrikethrough",
+			"btnSuperscript", "btnFixed", "btnQuote", "btnCode", "btnHr",
+			"btnOl", "btnUl", "btnInsertImage", "btnInsertLink",
+			"btnRemoveFormat" })
 	void onClick (ClickEvent event) {
 		if (txtContent != null) {
 			Widget sender = (Widget) event.getSource();
 
 			StringBuffer text = new StringBuffer(txtContent.getText());
-			int pos = txtContent.getCursorPos(), length = txtContent.getSelectionLength();
-			
+			String selection = txtContent.getSelectedText();
+			int pos = txtContent.getCursorPos(), length = selection.length();
+
 			if (sender == btnHeading) {
 				text.insert(pos, "#");
 				txtContent.setText(text.toString());
@@ -123,6 +113,80 @@ public class MarkdownToolbar extends Composite {
 				text.insert(pos, "*");
 				txtContent.setText(text.toString());
 				txtContent.setCursorPos(pos);
+			} else if (sender == btnStrikethrough) {
+				text.insert(pos + length, "~~");
+				text.insert(pos, "~~");
+				txtContent.setText(text.toString());
+				txtContent.setCursorPos(pos);
+			} else if (sender == btnSuperscript) {
+				text.insert(pos + length, "^");
+				text.insert(pos, "^");
+				txtContent.setText(text.toString());
+				txtContent.setCursorPos(pos);
+			} else if (sender == btnFixed) {
+				text.insert(pos + length, "`");
+				text.insert(pos, "`");
+				txtContent.setText(text.toString());
+				txtContent.setCursorPos(pos);
+			} else if (sender == btnCode) {
+				String language = Window.prompt("Enter code language:", "");
+				if (language != null) {
+					text.insert(pos + length, "\n```\n");
+					text.insert(pos, "\n```" + language + "\n");
+					txtContent.setText(text.toString());
+					txtContent.setCursorPos(pos);
+				}
+			} else if (sender == btnQuote) {
+				text.insert(pos, "\n>");
+				txtContent.setText(text.toString());
+				txtContent.setCursorPos(pos);
+			} else if (sender == btnHr) {
+				text.insert(pos, "\n\n---\n\n");
+				txtContent.setText(text.toString());
+				txtContent.setCursorPos(pos);
+			} else if (sender == btnUl) {
+				text.insert(pos, "- ");
+				txtContent.setText(text.toString());
+				txtContent.setCursorPos(pos);
+			} else if (sender == btnOl) {
+				text.insert(pos, "1. ");
+				txtContent.setText(text.toString());
+				txtContent.setCursorPos(pos);
+			} else if (sender == btnRemoveFormat) {
+				if (length > 0) {
+					selection = selection.replace("*", "").replace("~", "")
+							.replace("_", "").replace("^", "").replace("`", "")
+							.replace("-", "").replace("#", "");
+					text.replace(pos, pos + length, selection);
+					txtContent.setText(text.toString());
+					txtContent.setCursorPos(pos);
+				}
+			} else if (sender == btnInsertImage) {
+				String url = Window.prompt("Enter an image URL:", "http://");
+				if (url != null) {
+					if (length > 0) {
+						text.insert(pos + length, "](" + url + " \""
+								+ selection + "\")");
+						text.insert(pos, "![");
+					} else {
+						text.insert(pos, "![" + url + "](" + url + " \"\")");
+					}
+					txtContent.setText(text.toString());
+					txtContent.setCursorPos(pos);
+				}
+			} else if (sender == btnInsertLink) {
+				String url = Window.prompt("Enter a link URL:", "http://");
+				if (url != null) {
+					if (length > 0) {
+						text.insert(pos + length, "](" + url + " \""
+								+ selection + "\")");
+						text.insert(pos, "[");
+					} else {
+						text.insert(pos, "[" + url + "](" + url + " \"\")");
+					}
+					txtContent.setText(text.toString());
+					txtContent.setCursorPos(pos);
+				}
 			}
 		}
 	}
