7501202e566cd294ea1cef8b121c09246120e33b
billy1380 <william.shakour@gmail.com>
2015-06-30 13:37:17 +0100

    - added page fields to wizard page - added post lookup fields to wizard page

---
+++ src/main/java/com/willshex/blogwt/client/wizard/page/SelectPostWizardPage.java
@@ -0,0 +1,111 @@
+//
+//  SelectPostWizardPage.java
+//  blogwt
+//
+//  Created by William Shakour (billy1380) on 30 Jun 2015.
+//  Copyright © 2015 WillShex Limited. All rights reserved.
+//
+package com.willshex.blogwt.client.wizard.page;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Focusable;
+import com.google.gwt.user.client.ui.Widget;
+import com.willshex.blogwt.client.helper.PostHelper;
+import com.willshex.blogwt.client.wizard.WizardPage;
+import com.willshex.blogwt.shared.api.datatype.Post;
+
+/**
+ * @author William Shakour (billy1380)
+ *
+ */
+public class SelectPostWizardPage extends Composite implements WizardPage<Post> {
+
+	private static SelectPostWizardPageUiBinder uiBinder = GWT
+			.create(SelectPostWizardPageUiBinder.class);
+
+	interface SelectPostWizardPageUiBinder extends
+			UiBinder<Widget, SelectPostWizardPage> {}
+
+	private Post post;
+
+	public SelectPostWizardPage () {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see com.willshex.blogwt.client.wizard.WizardPage#isRepeatable() */
+	@Override
+	public boolean isRepeatable () {
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see com.willshex.blogwt.client.wizard.WizardPage#getData() */
+	@Override
+	public Post getData () {
+		return post;
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see
+	 * com.willshex.blogwt.client.wizard.WizardPage#setData(java.lang.Object) */
+	@Override
+	public void setData (Post data) {
+		post = data;
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see com.willshex.blogwt.client.wizard.WizardPage#getPageTitle() */
+	@Override
+	public String getPageTitle () {
+		return "Add Post";
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see com.willshex.blogwt.client.wizard.WizardPage#getPageDescription() */
+	@Override
+	public String getPageDescription () {
+		return PostHelper
+				.makeMarkup("Enter the id or slug of an existing post to add it as a section on the page.");
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see com.willshex.blogwt.client.wizard.WizardPage#getBody() */
+	@Override
+	public Widget getBody () {
+		return this;
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see com.willshex.blogwt.client.wizard.WizardPage#another() */
+	@Override
+	public WizardPage<?> another () {
+		return new SelectPostWizardPage();
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see com.willshex.blogwt.client.wizard.WizardPage#validate() */
+	@Override
+	public boolean validate () {
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see com.willshex.blogwt.client.wizard.WizardPage#getAutoFocusField() */
+	@Override
+	public Focusable getAutoFocusField () {
+		return null;
+	}
+
+}
