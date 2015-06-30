b889de1ee48dc2032d4f986974adbd231981b628
billy1380 <william.shakour@gmail.com>
2015-06-30 16:54:12 +0100

    added create and delete page api methods

--- src/main/java/com/willshex/blogwt/server/api/page/PageJsonServlet.java
+++ src/main/java/com/willshex/blogwt/server/api/page/PageJsonServlet.java
@@ -8,6 +8,8 @@
 package com.willshex.blogwt.server.api.page;
 
 import com.google.gson.JsonObject;
+import com.willshex.blogwt.shared.api.page.call.CreatePageRequest;
+import com.willshex.blogwt.shared.api.page.call.DeletePageRequest;
 import com.willshex.blogwt.shared.api.page.call.GetPageRequest;
 import com.willshex.blogwt.shared.api.page.call.GetPagesRequest;
 import com.willshex.gson.json.service.server.JsonServlet;
@@ -18,14 +20,22 @@ public final class PageJsonServlet extends JsonServlet {
 	protected String processAction (String action, JsonObject request) {
 		String output = "null";
 		PageApi service = new PageApi();
-		if ("GetPage".equals(action)) {
-			GetPageRequest input = new GetPageRequest();
+		if ("DeletePage".equals(action)) {
+			DeletePageRequest input = new DeletePageRequest();
 			input.fromJson(request);
-			output = service.getPage(input).toString();
+			output = service.deletePage(input).toString();
+		} else if ("CreatePage".equals(action)) {
+			CreatePageRequest input = new CreatePageRequest();
+			input.fromJson(request);
+			output = service.createPage(input).toString();
 		} else if ("GetPages".equals(action)) {
 			GetPagesRequest input = new GetPagesRequest();
 			input.fromJson(request);
 			output = service.getPages(input).toString();
+		} else if ("GetPage".equals(action)) {
+			GetPageRequest input = new GetPageRequest();
+			input.fromJson(request);
+			output = service.getPage(input).toString();
 		}
 		return output;
 	}
