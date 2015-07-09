18e9cbfffa76b57532f3029c00e4685b6d113a1e
billy1380 <william.shakour@gmail.com>
2015-07-09 17:35:00 +0100

    added page aware mechanism of creating nav items and highlighting selected pages

--- src/main/java/com/willshex/blogwt/client/part/HeaderPart.java
+++ src/main/java/com/willshex/blogwt/client/part/HeaderPart.java
@@ -8,21 +8,24 @@
 package com.willshex.blogwt.client.part;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.AnchorElement;
+import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
-import com.google.gwt.dom.client.ImageElement;
-import com.google.gwt.dom.client.SpanElement;
-import com.google.gwt.dom.client.Style.Display;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.safehtml.client.SafeHtmlTemplates;
+import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.gwt.safehtml.shared.SafeUri;
+import com.google.gwt.safehtml.shared.UriUtils;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.InlineHyperlink;
 import com.google.gwt.user.client.ui.Widget;
 import com.willshex.blogwt.client.DefaultEventBus;
 import com.willshex.blogwt.client.Resources;
@@ -33,9 +36,9 @@ import com.willshex.blogwt.client.controller.PropertyController;
 import com.willshex.blogwt.client.controller.SessionController;
 import com.willshex.blogwt.client.event.NavigationChangedEventHandler;
 import com.willshex.blogwt.client.page.PageType;
+import com.willshex.blogwt.shared.api.datatype.Page;
 import com.willshex.blogwt.shared.api.datatype.Session;
 import com.willshex.blogwt.shared.api.datatype.User;
-import com.willshex.blogwt.shared.api.helper.UserHelper;
 import com.willshex.blogwt.shared.api.user.call.LoginRequest;
 import com.willshex.blogwt.shared.api.user.call.LoginResponse;
 import com.willshex.blogwt.shared.api.user.call.LogoutRequest;
@@ -47,32 +50,92 @@ import com.willshex.gson.json.service.shared.StatusType;
 public class HeaderPart extends Composite implements LoginEventHandler,
 		LogoutEventHandler, NavigationChangedEventHandler {
 
-	private List<HandlerRegistration> registration;
-
-	@UiField Element elName;
-	@UiField InlineHyperlink btnAccount;
-	@UiField InlineHyperlink btnSignInOut;
-	@UiField AnchorElement btnHome;
-	@UiField AnchorElement btnPosts;
-	@UiField AnchorElement btnPages;
-	@UiField AnchorElement btnProperties;
-	@UiField AnchorElement btnUsers;
-	@UiField SpanElement spnUserName;
-
-	@UiField ImageElement imgAvatar;
-
 	private static HeaderPartUiBinder uiBinder = GWT
 			.create(HeaderPartUiBinder.class);
 
 	interface HeaderPartUiBinder extends UiBinder<Widget, HeaderPart> {}
+
+	interface HeaderTemplates extends SafeHtmlTemplates {
+		HeaderTemplates INSTANCE = GWT.create(HeaderTemplates.class);
+
+		@Template("<a href=\"{0}\">{1}</a>")
+		SafeHtml item (SafeUri href, String title);
+
+		@Template("<a href=\"{0}\"><span class=\"glyphicon glyphicon-{1}\"></span>{2}</a>")
+		SafeHtml glyphItem (SafeUri href, String glyphName, String title);
+
+		@Template("<a href=\"{0}\"><span>{1}</span><img class=\"img-circle\" src=\"{2}\" /></a>")
+		SafeHtml imageItem (SafeUri href, String src, String title);
+	}
+
+	@UiField Element elName;
+	@UiField Element elNavLeft;
+	@UiField Element elNavRight;
+
+	@UiField AnchorElement btnHome;
+	private Map<String, Element> items;
+
+	private Map<String, Element> ensureItems () {
+		if (items == null) {
+			items = new HashMap<String, Element>();
+		}
+
+		return items;
+	}
+
+	private List<HandlerRegistration> registration;
 
 	public HeaderPart () {
 		initWidget(uiBinder.createAndBindUi(this));
 
 		Resources.RES.styles().ensureInjected();
 
+		setupNavBarPages();
+	}
+
+	private void setupNavBarPages () {
 		elName.setInnerText(PropertyController.get().title());
-		btnHome.setHref(PageController.get().homePageUri());
+
+		boolean foundBrandPage = false;
+		List<Page> pages;
+		if ((pages = PageController.get().getHeaderPages()) != null) {
+			for (Page page : pages) {
+				if (page.parent != null) continue;
+
+				if (page.priority != null && page.priority.floatValue() == 0.0f) {
+					btnHome.setHref("#!" + page.slug);
+					foundBrandPage = true;
+				} else {
+					addItem(elNavLeft, page.title,
+							UriUtils.fromString("#!" + page.slug));
+				}
+			}
+		}
+
+		if (foundBrandPage) {
+			addItem(elNavLeft, "Blog", PageType.PostsPageType.asHref());
+		} else {
+			btnHome.setHref(PageType.PostsPageType.asHref());
+		}
+	}
+
+	private void addItem (Element parent, String title, SafeUri href) {
+		Element element = Document.get().createLIElement();
+		element.setInnerSafeHtml(HeaderTemplates.INSTANCE.item(href, title));
+		parent.appendChild(element);
+		ensureItems().put(href.asString().replaceFirst("#", ""), element);
+	}
+
+	private void activateItem (String page, boolean active) {
+		Element el = ensureItems().get(page);
+
+		if (el != null) {
+			if (active && !el.hasClassName("active")) {
+				el.addClassName("active");
+			} else if (!active && el.hasClassName("active")) {
+				el.removeClassName("active");
+			}
+		}
 	}
 
 	@UiHandler("btnExpand")
@@ -121,27 +184,27 @@ public class HeaderPart extends Composite implements LoginEventHandler,
 	 * @param user
 	 */
 	private void setLoggedInUser (User user) {
-		imgAvatar.setAlt(user.username);
-		imgAvatar.setSrc(user.avatar + "?s=" + UserHelper.AVATAR_HEADER_SIZE
-				+ "&default=retro");
-		spnUserName.setInnerText(user.forename + " " + user.surname);
+		//		imgAvatar.setAlt(user.username);
+		//		imgAvatar.setSrc(user.avatar + "?s=" + UserHelper.AVATAR_HEADER_SIZE
+		//				+ "&default=retro");
+		//		spnUserName.setInnerText(user.forename + " " + user.surname);
 		configure(true);
 	}
 
 	private void configure (boolean login) {
-		btnAccount.setVisible(login);
-		Display d = login ? Display.BLOCK : Display.NONE;
-		btnPages.getStyle().setDisplay(d);
-		btnProperties.getStyle().setDisplay(d);
-		btnUsers.getStyle().setDisplay(d);
-		btnSignInOut
-				.getElement()
-				.setInnerHTML(
-						login ? "<span class=\"glyphicon glyphicon-log-out\"></span> Sign Out"
-								: "<span class=\"glyphicon glyphicon-log-in\"></span> Sign In");
-		btnSignInOut.setTargetHistoryToken(login ? PageType.LogoutPageType
-				.asTargetHistoryToken() : PageType.LoginPageType
-				.asTargetHistoryToken());
+		//		btnAccount.setVisible(login);
+		//		Display d = login ? Display.BLOCK : Display.NONE;
+		//		btnPages.getStyle().setDisplay(d);
+		//		btnProperties.getStyle().setDisplay(d);
+		//		btnUsers.getStyle().setDisplay(d);
+		//		btnSignInOut
+		//				.getElement()
+		//				.setInnerHTML(
+		//						login ? "<span class=\"glyphicon glyphicon-log-out\"></span> Sign Out"
+		//								: "<span class=\"glyphicon glyphicon-log-in\"></span> Sign In");
+		//		btnSignInOut.setTargetHistoryToken(login ? PageType.LogoutPageType
+		//				.asTargetHistoryToken() : PageType.LoginPageType
+		//				.asTargetHistoryToken());
 	}
 
 	/* (non-Javadoc)
@@ -193,41 +256,10 @@ public class HeaderPart extends Composite implements LoginEventHandler,
 	 * com.willshex.blogwt.client.controller.NavigationController.Stack) */
 	@Override
 	public void navigationChanged (Stack previous, Stack current) {
-		btnAccount.getElement().getParentElement().removeClassName("active");
-		btnPosts.getParentElement().removeClassName("active");
-		btnPages.getParentElement().removeClassName("active");
-		btnProperties.getParentElement().removeClassName("active");
-		btnUsers.getParentElement().removeClassName("active");
-		btnSignInOut.getElement().getParentElement().removeClassName("active");
-
-		PageType p = PageType.fromString(current.getPage());
-
-		// this causes the blog heading to be selected by default
-		if (p == null) {
-			p = PageType.PostsPageType;
+		if (previous != null) {
+			activateItem(previous.getPage(), false);
 		}
 
-		switch (p) {
-		case ChangeDetailsPageType:
-			btnAccount.getElement().getParentElement().addClassName("active");
-			break;
-		case PagesPageType:
-			btnPages.getParentElement().addClassName("active");
-			break;
-		case UsersPageType:
-			btnUsers.getParentElement().addClassName("active");
-			break;
-		case LoginPageType:
-			btnSignInOut.getElement().getParentElement().addClassName("active");
-			break;
-		case PostsPageType:
-			btnPosts.getParentElement().addClassName("active");
-			break;
-		case PropertiesPageType:
-			btnProperties.getParentElement().addClassName("active");
-			break;
-		default:
-			break;
-		}
+		activateItem(current.getPage(), true);
 	}
 }
