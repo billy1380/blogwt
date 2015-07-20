2eef397cbb9571213baec713b28ffc02f338e208
billy1380 <william.shakour@gmail.com>
2015-07-20 18:22:49 +0100

    added handler that fires when a property changes

--- src/main/java/com/willshex/blogwt/client/part/property/StringPropertyPart.java
+++ src/main/java/com/willshex/blogwt/client/part/property/StringPropertyPart.java
@@ -9,9 +9,10 @@ package com.willshex.blogwt.client.part.property;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
@@ -21,7 +22,7 @@ import com.willshex.blogwt.client.helper.UiHelper;
  * @author William Shakour (billy1380)
  *
  */
-public class StringPropertyPart extends Composite {
+public class StringPropertyPart extends AbstractPropertyPart {
 
 	@UiField Element elDescription;
 	@UiField Element elName;
@@ -54,20 +55,43 @@ public class StringPropertyPart extends Composite {
 	}
 
 	/**
-	 * @param value
-	 */
-	public void setValue (String value) {
-		txtValue.setValue(value);
-	}
-
-	/**
 	 */
 	public void setAutofocus () {
 		UiHelper.autoFocus(txtValue);
 	}
 
+	@UiHandler("txtValue")
+	void onTextValueChanged (ValueChangeEvent<String> vce) {
+		setValue(vce.getValue(), true);
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see com.google.gwt.user.client.ui.HasValue#setValue(java.lang.Object,
+	 * boolean) */
+	@Override
+	public void setValue (String value, boolean fireEvents) {
+		if (value == null) {
+			value = "";
+		}
+
+		String oldValue = getValue();
+
+		txtValue.setValue(value);
+		this.value = value;
+
+		if (value.equals(oldValue)) { return; }
+		if (fireEvents) {
+			ValueChangeEvent.fire(this, value);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see com.google.gwt.user.client.ui.HasValue#getValue() */
+	@Override
 	public String getValue () {
-		return txtValue.getValue();
+		return value;
 	}
 
 }
