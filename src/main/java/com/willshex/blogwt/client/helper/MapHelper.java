095e06aa64e5d47d59554906c810eadb2f4204cc
billy1380 <william.shakour@gmail.com>
2015-07-22 12:05:12 +0100

    parameters for map are passed via plugin parameter

--- src/main/java/com/willshex/blogwt/client/helper/MapHelper.java
+++ src/main/java/com/willshex/blogwt/client/helper/MapHelper.java
@@ -29,20 +29,69 @@ public class MapHelper {
 			Map<String, String> params) {
 		Style s = element.getStyle();
 
-		// TODO: get these values from the parameters or lines
-		s.setWidth(100.0, Unit.PCT);
-		s.setHeight(200.0, Unit.PX);
+		float width = floatParam(params, "width", 100);
+		Unit widthUnits = unitParam(params, "widthUnits", Unit.PCT);
+		s.setWidth(width, widthUnits);
 
-		showMap(element, -34.397f, 150.644f, 8);
+		float height = floatParam(params, "height", 200);
+		Unit heightUnits = unitParam(params, "heightUnits", Unit.PT);
+		s.setHeight(height, heightUnits);
+
+		float lat = floatParam(params, "lat", -34.397f);
+		float lng = floatParam(params, "lng", 150.644f);
+
+		int zoom = intParam(params, "zoom", 8);
+
+		String markerName = params.get("markerName");
+
+		showMap(element, lat, lng, zoom, markerName);
+	}
+
+	private static int intParam (Map<String, String> params, String name,
+			int defaultValue) {
+		String paramValue = params.get(name);
+		int value = defaultValue;
+		if (paramValue != null) {
+			try {
+				value = Integer.valueOf(paramValue).intValue();
+			} catch (Exception ex) {}
+		}
+
+		return value;
+	}
+
+	public static float floatParam (Map<String, String> params, String name,
+			float defaultValue) {
+		String paramValue = params.get(name);
+		float value = defaultValue;
+		if (paramValue != null) {
+			try {
+				value = Float.valueOf(paramValue).floatValue();
+			} catch (Exception ex) {}
+		}
+
+		return value;
+	}
+
+	public static Unit unitParam (Map<String, String> params, String name,
+			Unit defaultValue) {
+		String paramUnit = params.get(name);
+		Unit units = defaultValue;
+		if (paramUnit != null) {
+			try {
+				units = Unit.valueOf(paramUnit);
+			} catch (Exception ex) {}
+		}
+
+		return units;
 	}
 
 	private static native void showMap (Element element, float lat, float lng,
-			float zoom) /*-{
-						var mapOptions = {
-						zoom: zoom,
-						center: {lat: lat, lng: lng}
-						};
-						var map = new google.maps.Map(element, mapOptions);
-						}-*/;
+			int zoom, String markerName) /*-{
+											var myLatlng = new google.maps.LatLng(lat,lng);
+											var mapOptions = {zoom:zoom,center:myLatlng};
+											var map = new google.maps.Map(element,mapOptions);
+											if (markerName !== undefined) {var marker = new google.maps.Marker({position: myLatlng,map: map,title:(markerName === "<nameless>" ? undefined : markerName)});}
+											}-*/;
 
 }
