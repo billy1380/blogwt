77a2ded9d8e00eb1c43b3b806de561f1ddf77a4a
billy1380 <william.shakour@gmail.com>
2015-07-24 18:00:22 +0100

    added more property names to work on

--- src/main/java/com/willshex/blogwt/shared/helper/PropertyHelper.java
+++ src/main/java/com/willshex/blogwt/shared/helper/PropertyHelper.java
@@ -19,15 +19,43 @@ import com.willshex.blogwt.shared.api.datatype.Property;
  *
  */
 public class PropertyHelper {
+	// setup properties
 	public static final String TITLE = "setup.title";
 	public static final String EXTENDED_TITLE = "setup.extended.title";
 	public static final String COPYRIGHT_HOLDER = "setup.copyright.holder";
 	public static final String COPYRIGHT_URL = "setup.copyright.url";
+	
+	public static final String PASSWORD_HASH_SALT = "setup.password.hash.salt";
 
+	// functional properties
 	public static final String MARKDOWN_MAPS_API_KEY = "markdown.maps.api.key";
+	public static final String MARKDOWN_PREFETCH_INCLUDES = "markdown.include.prefectch";
+	
 	public static final String POST_COMMENTS_ENABLED = "post.comments.enabled";
 	public static final String POST_DISQUS_ID = "post.comments.disqus.id";
 	public static final String POST_CATEGORY_ID = "post.comments.disqus.category.id";
+	
+	public static final String POST_ENABLE_EMOJI = "post.enable.emoji";
+	public static final String POST_SHOW_AUTHOR = "post.show.author";
+	public static final String POST_SHOW_AUTHOR_SUMMARY = "post.show.author.summary";
+	
+	
+	public static final String SHOW_SIGN_IN = "header.show.signin";
+	
+	public static final String GENERATE_RSS_FEED = "content.rss.generate";
+	public static final String SHORT_DESCRIPTION = "content.short.description";
+	
+	public static final String SMALL_LOGO_URL = "content.small.logo.url";
+	public static final String LARGE_LOGO_URL = "content.large_logo_url";
+	
+	public static final String EMOJI_THEME = "content.emoji.theme";
+	
+	public static final String FOOTER_SHOW_VERSION= "footer.show.site.version";
+	
+	// user properties
+	public static final String ALLOW_USER_REGISTRATION = "user.allow.registration";
+	
+	
 
 	public static final String TITLE_DESCRIPTION = "Blog Title";
 	public static final String EXTENDED_TITLE_DESCRIPTION = "Extended Title (Short Description)";
