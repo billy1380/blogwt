0e9988f63f251709dc668dcba373a5d45d50e62c
billy1380 <william.shakour@gmail.com>
2015-08-26 20:13:20 +0100

    initial attempt at archive model showing post ids

--- src/main/java/com/willshex/blogwt/client/part/ArchivePart.java
+++ src/main/java/com/willshex/blogwt/client/part/ArchivePart.java
@@ -9,8 +9,11 @@ package com.willshex.blogwt.client.part;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.cellview.client.CellTree;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.willshex.blogwt.client.controller.ArchiveEntryController;
 
 /**
  * @author William Shakour (billy1380)
@@ -23,8 +26,12 @@ public class ArchivePart extends Composite {
 
 	interface ArchivePartUiBinder extends UiBinder<Widget, ArchivePart> {}
 
-	public ArchivePart () {
-		initWidget(uiBinder.createAndBindUi(this));
-	}
+	@UiField(provided = true) CellTree ctArchiveEntries;
 
+	public ArchivePart () {
+		ctArchiveEntries = new CellTree(
+				new ArchiveEntryController.ArchiveModel(), null);
+		initWidget(uiBinder.createAndBindUi(this));
+		ctArchiveEntries.setAnimationEnabled(true);
+	}
 }
