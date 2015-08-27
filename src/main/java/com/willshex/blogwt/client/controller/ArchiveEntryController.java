6320d0a35e06f2eb214fd214ce56712a5dc19df1
billy1380 <william.shakour@gmail.com>
2015-08-27 18:00:50 +0100

    very messy but working version of archive model

--- src/main/java/com/willshex/blogwt/client/controller/ArchiveEntryController.java
+++ src/main/java/com/willshex/blogwt/client/controller/ArchiveEntryController.java
@@ -15,11 +15,16 @@ import java.util.Map;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
 import com.google.gwt.cell.client.AbstractCell;
-import com.google.gwt.cell.client.TextCell;
+import com.google.gwt.cell.client.AbstractSafeHtmlCell;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.i18n.client.LocaleInfo;
+import com.google.gwt.safehtml.client.SafeHtmlTemplates;
+import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.google.gwt.text.shared.AbstractSafeHtmlRenderer;
 import com.google.gwt.view.client.ListDataProvider;
 import com.google.gwt.view.client.TreeViewModel;
+import com.willshex.blogwt.client.cell.blog.PostSummaryCell;
 import com.willshex.blogwt.shared.api.datatype.ArchiveEntry;
 import com.willshex.blogwt.shared.api.datatype.Post;
 
@@ -38,13 +43,90 @@ public class ArchiveEntryController extends ListDataProvider<ArchiveEntry> {
 		return one;
 	}
 
+	public interface TextBadgeTemplate extends SafeHtmlTemplates {
+		TextBadgeTemplate INSTANCE = GWT.create(TextBadgeTemplate.class);
+
+		@Template("{0} <span class=\"badge\">{1}</span>")
+		SafeHtml textWithBadge (String name, int count);
+	}
+
+	public static final class ArchiveModelItem {
+		private Integer year;
+		private Integer month;
+		private Integer count;
+		private Post post;
+
+		/**
+		 * @param post
+		 */
+		public ArchiveModelItem (Post post) {
+			this.post = post;
+		}
+
+		/**
+		 * @param year
+		 * @param count
+		 */
+		public ArchiveModelItem (Integer value, Integer count, boolean isYear) {
+			if (isYear) {
+				year = value;
+			} else {
+				month = value;
+			}
+
+			this.count = count;
+		}
+
+		public boolean isYear () {
+			return year != null;
+		}
+
+		public boolean isMonth () {
+			return month != null;
+		}
+
+		public boolean isPost () {
+			return post != null;
+		}
+
+		/**
+		 * @return the year
+		 */
+		public Integer getYear () {
+			return year;
+		}
+
+		/**
+		 * @return the month
+		 */
+		public Integer getMonth () {
+			return month;
+		}
+
+		/**
+		 * @return the count
+		 */
+		public Integer getCount () {
+			return count;
+		}
+
+		/**
+		 * @return the post
+		 */
+		public Post getPost () {
+			return post;
+		}
+
+	}
+
 	public static final class ArchiveModel implements TreeViewModel {
 
 		private Map<Integer, List<ArchiveEntry>> years = new HashMap<Integer, List<ArchiveEntry>>();
 		private Map<Integer, Integer> yearPostCount = new HashMap<Integer, Integer>();
 		private static final String[] months = LocaleInfo.getCurrentLocale()
 				.getDateTimeFormatInfo().monthsFull();
-		private final static TextCell PROTOTYPE = new TextCell();
+
+		private static final PostSummaryCell POST_CELL = new PostSummaryCell();
 
 		/* (non-Javadoc)
 		 * 
@@ -52,7 +134,7 @@ public class ArchiveEntryController extends ListDataProvider<ArchiveEntry> {
 		 * com.google.gwt.view.client.TreeViewModel#getNodeInfo(java.lang.Object
 		 * ) */
 		@Override
-		public <T> NodeInfo<?> getNodeInfo (T value) {
+		public <T> NodeInfo<?> getNodeInfo (final T value) {
 			NodeInfo<?> info = null;
 
 			if (value == null) {
@@ -76,52 +158,67 @@ public class ArchiveEntryController extends ListDataProvider<ArchiveEntry> {
 
 				info = new DefaultNodeInfo<Integer>(
 						new ListDataProvider<Integer>(new ArrayList<Integer>(
-								years.keySet())), new AbstractCell<Integer>() {
+								years.keySet())),
+						new AbstractSafeHtmlCell<Integer>(
+								new AbstractSafeHtmlRenderer<Integer>() {
+
+									@Override
+									public SafeHtml render (Integer value) {
+										SafeHtmlBuilder builder = new SafeHtmlBuilder();
+										builder.append(TextBadgeTemplate.INSTANCE
+												.textWithBadge(
+														value.toString(),
+														yearPostCount
+																.get(value)
+																.intValue()));
+										return builder.toSafeHtml();
+									}
+								}) {
 
 							@Override
-							public void render (
+							protected void render (
 									com.google.gwt.cell.client.Cell.Context context,
-									Integer value, SafeHtmlBuilder sb) {
-								PROTOTYPE.render(
-										context,
-										value.toString() + " ("
-												+ yearPostCount.get(value)
-												+ ")", sb);
+									SafeHtml data, SafeHtmlBuilder sb) {
+								sb.append(data);
 							}
-
 						});
 			} else if (value instanceof Integer) {
 				info = new DefaultNodeInfo<ArchiveEntry>(
 						new ListDataProvider<ArchiveEntry>(
 								years.get((Integer) value)),
-						new AbstractCell<ArchiveEntry>() {
+						new AbstractSafeHtmlCell<ArchiveEntry>(
+								new AbstractSafeHtmlRenderer<ArchiveEntry>() {
+
+									@Override
+									public SafeHtml render (ArchiveEntry value) {
+										SafeHtmlBuilder builder = new SafeHtmlBuilder();
+										builder.append(TextBadgeTemplate.INSTANCE
+												.textWithBadge(
+														months[value.month
+																.intValue()],
+														value.posts.size()));
+										return builder.toSafeHtml();
+									}
+								}) {
 
 							@Override
-							public void render (
+							protected void render (
 									com.google.gwt.cell.client.Cell.Context context,
-									ArchiveEntry value, SafeHtmlBuilder sb) {
-								PROTOTYPE.render(context,
-										months[value.month.intValue()] + " ("
-												+ value.posts.size() + ")", sb);
+									SafeHtml data, SafeHtmlBuilder sb) {
+								sb.append(data);
 							}
-
 						});
 			} else if (value instanceof ArchiveEntry) {
-				info = new DefaultNodeInfo<Post>(new ListDataProvider<Post>(
-						((ArchiveEntry) value).posts),
+				info = new DefaultNodeInfo<Post>(
+						PostController.archived((ArchiveEntry) value),
 						new AbstractCell<Post>() {
-
 							@Override
 							public void render (
 									com.google.gwt.cell.client.Cell.Context context,
 									Post value, SafeHtmlBuilder sb) {
-								PROTOTYPE.render(context, value.id.toString(),
-										sb);
+								POST_CELL.render(context, value, sb);
 							}
-
 						});
-			} else if (value instanceof Post) {
-
 			}
 
 			return info;
