405c948dde6ee6efa11fad2c7ceec61877922c5c
billy1380 <william.shakour@gmail.com>
2015-09-03 12:16:39 +0100

    added ability to preserve case when splitting tags

--- src/main/java/com/willshex/blogwt/shared/helper/TagHelper.java
+++ src/main/java/com/willshex/blogwt/shared/helper/TagHelper.java
@@ -4,20 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TagHelper {
+
 	/**
-	 * Converts a list of comma delimited tags into a tag list
+	 * Converts a list of comma delimited tags into a tag list. Tags are all made lowercase.
 	 * 
 	 * @param tags
 	 * @return
 	 */
 	public static List<String> convertToTagList (String tags) {
+		return convertToTagList(tags, false);
+	}
+
+	/**
+	 * Converts a list of comma delimited tags into a tag list
+	 * 
+	 * @param tags
+	 * @param perserveCase
+	 * @return
+	 */
+	public static List<String> convertToTagList (String tags,
+			boolean perserveCase) {
 		List<String> tagList = null;
 
 		if (tags != null && tags.length() >= 0) {
 			String[] splitTags = tags.split(",");
 
 			for (String item : splitTags) {
-				String tag = item.trim().toLowerCase();
+				String tag = item.trim();
+
+				if (!perserveCase) {
+					tag = tag.toLowerCase();
+				}
 
 				if (tag.length() > 0) {
 					if (tagList == null) {
