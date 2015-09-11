6d96079fafcf9194fd10f5128a91f47db14cf4e1
billy1380 <william.shakour@gmail.com>
2015-09-11 11:11:16 +0100

    added cookie notice support

--- src/main/java/com/willshex/blogwt/client/part/CookieNoticePart.java
+++ src/main/java/com/willshex/blogwt/client/part/CookieNoticePart.java
@@ -8,9 +8,17 @@
 package com.willshex.blogwt.client.part;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Cookies;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.InlineHyperlink;
 import com.google.gwt.user.client.ui.Widget;
+import com.willshex.blogwt.client.controller.PropertyController;
+import com.willshex.blogwt.shared.helper.PropertyHelper;
 
 /**
  * @author William Shakour (billy1380)
@@ -24,8 +32,43 @@ public class CookieNoticePart extends Composite {
 	interface CookieNoticePartUiBinder extends
 			UiBinder<Widget, CookieNoticePart> {}
 
+	private static final String EU_COOKIE_NOTICE_KEY = "eu.cookie.notice";
+
+	@UiField Element elTitle;
+	@UiField InlineHyperlink btnSeeDetails;
+
 	public CookieNoticePart () {
 		initWidget(uiBinder.createAndBindUi(this));
+
+		elTitle.setInnerText(PropertyController.get().title());
+
+		String detailsPageSlug = PropertyController.get().stringProperty(
+				PropertyHelper.COOKIE_DETAILS_PAGE_SLUG);
+
+		if (detailsPageSlug == null
+				|| PropertyHelper.NONE_VALUE.equals(detailsPageSlug)) {
+			btnSeeDetails.setVisible(false);
+		} else {
+			btnSeeDetails.setTargetHistoryToken("!" + detailsPageSlug);
+		}
+
+		if (isCookie()) {
+			setVisible(false);
+		}
+	}
+
+	@UiHandler("btnGotIt")
+	void btnGotItClicked (ClickEvent ce) {
+		setCookie();
+		setVisible(false);
+	}
+
+	private boolean isCookie () {
+		return Cookies.getCookie(EU_COOKIE_NOTICE_KEY) != null;
+	}
+
+	private void setCookie () {
+		Cookies.setCookie(EU_COOKIE_NOTICE_KEY, "Got It!");
 	}
 
 }
