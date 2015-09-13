bde4921cc0ed2104f6a4c368fbd9499497bb40b4
billy1380 <william.shakour@gmail.com>
2015-09-13 15:34:08 +0100

    removed client helper methods

--- src/main/java/com/willshex/blogwt/client/page/PageType.java
+++ src/main/java/com/willshex/blogwt/client/page/PageType.java
@@ -12,32 +12,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.google.gwt.core.client.Scheduler;
-import com.google.gwt.core.client.Scheduler.ScheduledCommand;
-import com.google.gwt.safehtml.shared.SafeUri;
-import com.google.gwt.safehtml.shared.UriUtils;
-import com.google.gwt.user.client.History;
 import com.spacehopperstudios.utility.StringUtils;
-import com.willshex.blogwt.client.page.admin.PagesPage;
-import com.willshex.blogwt.client.page.admin.PermissionsPage;
-import com.willshex.blogwt.client.page.admin.PropertiesPage;
-import com.willshex.blogwt.client.page.admin.ResourcesPage;
-import com.willshex.blogwt.client.page.admin.RolesPage;
-import com.willshex.blogwt.client.page.admin.UsersPage;
-import com.willshex.blogwt.client.page.blog.EditPostPage;
-import com.willshex.blogwt.client.page.blog.PostDetailPage;
-import com.willshex.blogwt.client.page.blog.PostsPage;
-import com.willshex.blogwt.client.page.blog.SearchPage;
-import com.willshex.blogwt.client.page.blog.SetupBlogPage;
-import com.willshex.blogwt.client.page.blog.TagPage;
-import com.willshex.blogwt.client.page.page.EditPagePage;
-import com.willshex.blogwt.client.page.page.PageDetailPage;
-import com.willshex.blogwt.client.page.user.ChangeDetailsPage;
-import com.willshex.blogwt.client.page.user.ChangePasswordPage;
-import com.willshex.blogwt.client.page.user.LoginPage;
-import com.willshex.blogwt.client.page.user.RegisterPage;
-import com.willshex.blogwt.client.page.user.ResetPasswordPage;
-import com.willshex.blogwt.client.page.user.VerifyAccountPage;
 import com.willshex.blogwt.shared.api.datatype.Permission;
 import com.willshex.blogwt.shared.helper.PermissionHelper;
 import com.willshex.blogwt.shared.helper.RoleHelper;
@@ -72,7 +47,6 @@ public enum PageType {
 
 	private String value;
 	private static Map<String, PageType> valueLookup = null;
-	private Page defaultPage = null;
 	private Map<String, Permission> requiredPermissions;
 	private boolean requiresAuthentication;
 
@@ -119,14 +93,6 @@ public enum PageType {
 		return asString;
 	}
 
-	public SafeUri asHref () {
-		return UriUtils.fromString("#!" + toString());
-	}
-
-	public SafeUri asHref (String... params) {
-		return UriUtils.fromString("#!" + toString(params));
-	}
-
 	public String asTargetHistoryToken () {
 		return "!" + toString();
 	}
@@ -169,113 +135,11 @@ public enum PageType {
 		return this.value.equals(value);
 	}
 
-	/**
-	 * @return
-	 */
-	public Page create () {
-		Page page = null;
-
-		switch (this) {
-		case UsersPageType:
-			page = new UsersPage();
-			break;
-		case LoginPageType:
-			page = new LoginPage();
-			break;
-		case RegisterPageType:
-			page = new RegisterPage();
-			break;
-		case ChangePasswordPageType:
-			page = new ChangePasswordPage();
-			break;
-		case ResetPasswordPageType:
-			page = new ResetPasswordPage();
-			break;
-		case RolesPageType:
-			page = new RolesPage();
-			break;
-		case PermissionsPageType:
-			page = new PermissionsPage();
-			break;
-		case ChangeDetailsPageType:
-			page = new ChangeDetailsPage();
-			break;
-		case SetupBlogPageType:
-			page = new SetupBlogPage();
-			break;
-		case EditPostPageType:
-			page = new EditPostPage();
-			break;
-		case PostDetailPageType:
-			page = new PostDetailPage();
-			break;
-		case PropertiesPageType:
-			page = new PropertiesPage();
-			break;
-		case ResourcesPageType:
-			page = new ResourcesPage();
-			break;
-		case PagesPageType:
-			page = new PagesPage();
-			break;
-		case EditPagePageType:
-			page = new EditPagePage();
-			break;
-		case TagPostsPageType:
-			page = new TagPage();
-			break;
-		case LogoutPageType:
-		case PostsPageType:
-			page = new PostsPage();
-			break;
-		case SearchPostsPageType:
-			page = new SearchPage();
-			break;
-		case VerifyAccountPageType:
-			page = new VerifyAccountPage();
-			break;
-		case PageDetailPageType:
-		default:
-			if (defaultPage == null) {
-				defaultPage = new PageDetailPage();
-			}
-			page = defaultPage;
-			break;
-		}
-
-		return page;
-	}
-
-	public void show () {
-		Scheduler.get().scheduleDeferred(new ScheduledCommand() {
-			@Override
-			public void execute () {
-				History.newItem(asTargetHistoryToken());
-			}
-		});
-	}
-
-	public void show (final String... params) {
-		Scheduler.get().scheduleDeferred(new ScheduledCommand() {
-			@Override
-			public void execute () {
-				History.newItem(asTargetHistoryToken(params));
-			}
-		});
-	}
-
 	private static String stripExclamation (String value) {
 		if (value != null && value.length() > 0 && value.charAt(0) == '!') {
 			value = value.substring(1);
 		}
 
 		return value;
-	}
-
-	/**
-	 * @return
-	 */
-	public static PageType home () {
-		return PostsPageType;
 	}
 }
