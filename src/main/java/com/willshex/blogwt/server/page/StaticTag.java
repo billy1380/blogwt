7cc944b05d4b039354861bd13a3779bfdd608982
billy1380 <william.shakour@gmail.com>
2015-09-22 08:32:59 +0100

    Added implementation of tag page

--- src/main/java/com/willshex/blogwt/server/page/StaticTag.java
+++ src/main/java/com/willshex/blogwt/server/page/StaticTag.java
@@ -7,7 +7,20 @@
 //
 package com.willshex.blogwt.server.page;
 
+import org.markdown4j.server.IncludePlugin;
+import org.markdown4j.server.MarkdownProcessor;
+
+import com.willshex.blogwt.server.api.blog.BlogApi;
+import com.willshex.blogwt.server.helper.UserHelper;
+import com.willshex.blogwt.shared.api.blog.call.GetPostsRequest;
+import com.willshex.blogwt.shared.api.blog.call.GetPostsResponse;
+import com.willshex.blogwt.shared.api.datatype.Post;
+import com.willshex.blogwt.shared.helper.DateTimeHelper;
+import com.willshex.blogwt.shared.helper.PagerHelper;
+import com.willshex.blogwt.shared.helper.PostHelper;
+import com.willshex.blogwt.shared.page.PageType;
 import com.willshex.blogwt.shared.page.Stack;
+import com.willshex.gson.json.service.shared.StatusType;
 
 /**
  * @author William Shakour (billy1380)
@@ -26,7 +39,58 @@ class StaticTag extends StaticTemplate {
 	 * StringBuffer) */
 	@Override
 	protected void appendPage (StringBuffer markup) {
+		String tag;
+		if ((tag = stack.getAction()) != null) {
+			markup.append("<h2>Posts tagged with <strong>&apos;" + tag
+					+ "&apos;</strong></h2>");
 
+			BlogApi api = new BlogApi();
+
+			GetPostsRequest input = input(GetPostsRequest.class).pager(
+					PagerHelper.createDefaultPager()).tag(tag);
+
+			GetPostsResponse output = api.getPosts(input);
+
+			if (output.status == StatusType.StatusTypeSuccess
+					&& output.posts != null) {
+				MarkdownProcessor processor = new MarkdownProcessor();
+				processor.registerPlugins(new IncludePlugin());
+
+				String link, body;
+				for (Post post : output.posts) {
+					body = "Empty... :imp:";
+
+					if (post.summary != null && post.summary.length() > 0) {
+						body = post.summary;
+					}
+
+					link = "#"
+							+ PageType.PostDetailPageType
+									.asTargetHistoryToken(PostHelper
+											.getSlug(post));
+
+					markup.append("<div><a href=\"");
+					markup.append(link);
+					markup.append("\">");
+					markup.append(process("##" + post.title));
+					markup.append("</a><div><span>");
+					markup.append(DateTimeHelper.ago(post.published));
+					markup.append("</span> by <img src=\"");
+					markup.append(post.author.avatar);
+					markup.append("?s=");
+					markup.append(UserHelper.AVATAR_HEADER_SIZE);
+					markup.append("&default=retro\" /> ");
+					markup.append(UserHelper.handle(post.author));
+					markup.append("</div><div>");
+					markup.append(process(body));
+					markup.append("</div><a href=\"");
+					markup.append(link);
+					markup.append("\">Read More</a></div>");
+				}
+			} else {
+				markup.append(output.error.toString());
+			}
+		}
 	}
 
 }
