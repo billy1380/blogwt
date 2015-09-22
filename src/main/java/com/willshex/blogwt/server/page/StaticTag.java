78e3479d500d26ae841588cac1a764b5d1e8be81
billy1380 <william.shakour@gmail.com>
2015-09-22 08:37:41 +0100

    refactored static posts so tags and posts uses the same code

--- src/main/java/com/willshex/blogwt/server/page/StaticTag.java
+++ src/main/java/com/willshex/blogwt/server/page/StaticTag.java
@@ -7,18 +7,10 @@
 //
 package com.willshex.blogwt.server.page;
 
-import org.markdown4j.server.IncludePlugin;
-import org.markdown4j.server.MarkdownProcessor;
-
 import com.willshex.blogwt.server.api.blog.BlogApi;
-import com.willshex.blogwt.server.helper.UserHelper;
 import com.willshex.blogwt.shared.api.blog.call.GetPostsRequest;
 import com.willshex.blogwt.shared.api.blog.call.GetPostsResponse;
-import com.willshex.blogwt.shared.api.datatype.Post;
-import com.willshex.blogwt.shared.helper.DateTimeHelper;
 import com.willshex.blogwt.shared.helper.PagerHelper;
-import com.willshex.blogwt.shared.helper.PostHelper;
-import com.willshex.blogwt.shared.page.PageType;
 import com.willshex.blogwt.shared.page.Stack;
 import com.willshex.gson.json.service.shared.StatusType;
 
@@ -26,7 +18,7 @@ import com.willshex.gson.json.service.shared.StatusType;
  * @author William Shakour (billy1380)
  *
  */
-class StaticTag extends StaticTemplate {
+class StaticTag extends StaticPosts {
 
 	public StaticTag (Stack stack) {
 		super(stack);
@@ -53,40 +45,7 @@ class StaticTag extends StaticTemplate {
 
 			if (output.status == StatusType.StatusTypeSuccess
 					&& output.posts != null) {
-				MarkdownProcessor processor = new MarkdownProcessor();
-				processor.registerPlugins(new IncludePlugin());
-
-				String link, body;
-				for (Post post : output.posts) {
-					body = "Empty... :imp:";
-
-					if (post.summary != null && post.summary.length() > 0) {
-						body = post.summary;
-					}
-
-					link = "#"
-							+ PageType.PostDetailPageType
-									.asTargetHistoryToken(PostHelper
-											.getSlug(post));
-
-					markup.append("<div><a href=\"");
-					markup.append(link);
-					markup.append("\">");
-					markup.append(process("##" + post.title));
-					markup.append("</a><div><span>");
-					markup.append(DateTimeHelper.ago(post.published));
-					markup.append("</span> by <img src=\"");
-					markup.append(post.author.avatar);
-					markup.append("?s=");
-					markup.append(UserHelper.AVATAR_HEADER_SIZE);
-					markup.append("&default=retro\" /> ");
-					markup.append(UserHelper.handle(post.author));
-					markup.append("</div><div>");
-					markup.append(process(body));
-					markup.append("</div><a href=\"");
-					markup.append(link);
-					markup.append("\">Read More</a></div>");
-				}
+				showPosts(output.posts, markup);
 			} else {
 				markup.append(output.error.toString());
 			}
