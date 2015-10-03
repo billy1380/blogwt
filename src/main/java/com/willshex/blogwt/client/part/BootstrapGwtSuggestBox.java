7c2580be2e0402ca631d928c82cfb84de316a337
billy1380 <william.shakour@gmail.com>
2015-10-03 09:56:38 +0100

    suggest box with gwt styles

---
+++ src/main/java/com/willshex/blogwt/client/part/BootstrapGwtSuggestBox.java
@@ -0,0 +1,26 @@
+//
+//  BootstrapGwtSuggestBox.java
+//  blogwt
+//
+//  Created by William Shakour (billy1380) on 3 Oct 2015.
+//  Copyright © 2015 WillShex Limited. All rights reserved.
+//
+package com.willshex.blogwt.client.part;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+
+/**
+ * @author William Shakour (billy1380)
+ *
+ */
+public interface BootstrapGwtSuggestBox extends ClientBundle {
+	public static final BootstrapGwtSuggestBox INSTANCE = GWT
+			.create(BootstrapGwtSuggestBox.class);
+
+	public interface BootstrapGwtSuggestBoxStyle extends CssResource {};
+
+	@Source("bootstrapgwtsuggestbox.gss")
+	BootstrapGwtSuggestBoxStyle styles ();
+}
