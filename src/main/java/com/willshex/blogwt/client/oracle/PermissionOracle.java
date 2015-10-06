644b3cd7efef2cb23e28b50f5bcd66c57433fe76
billy1380 <william.shakour@gmail.com>
2015-10-06 14:50:46 +0100

    call get roles and get permissions from oracles

--- src/main/java/com/willshex/blogwt/client/oracle/PermissionOracle.java
+++ src/main/java/com/willshex/blogwt/client/oracle/PermissionOracle.java
@@ -9,14 +9,24 @@ package com.willshex.blogwt.client.oracle;
 
 import java.util.Collections;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.willshex.blogwt.client.controller.SessionController;
+import com.willshex.blogwt.client.helper.ApiHelper;
 import com.willshex.blogwt.client.oracle.RoleOracle.NameDescriptionTemplates;
 import com.willshex.blogwt.shared.api.datatype.Permission;
+import com.willshex.blogwt.shared.api.user.call.GetPermissionsRequest;
+import com.willshex.blogwt.shared.api.user.call.GetPermissionsResponse;
+import com.willshex.blogwt.shared.helper.PagerHelper;
+import com.willshex.gson.json.service.shared.StatusType;
 
 /**
  * @author William Shakour (billy1380)
  *
  */
 public class PermissionOracle extends SuggestOracle<Permission> {
+
+	private com.google.gwt.http.client.Request getPermissionsRequest;
 
 	/* (non-Javadoc)
 	 * 
@@ -25,8 +35,41 @@ public class PermissionOracle extends SuggestOracle<Permission> {
 	 * .user.client.ui.SuggestOracle.Request,
 	 * com.google.gwt.user.client.ui.SuggestOracle.Callback) */
 	@Override
-	protected void lookup (Request request, Callback callback) {
-		this.foundItems(request, callback, Collections.<Permission> emptyList());
+	protected void lookup (final Request request, final Callback callback) {
+		final GetPermissionsRequest input = ApiHelper
+				.setAccessCode(new GetPermissionsRequest());
+		input.session = SessionController.get().sessionForApiCall();
+		input.query = request.getQuery();
+		input.pager = PagerHelper.createDefaultPager();
+		input.pager.count = Integer.valueOf(request.getLimit());
+
+		if (getPermissionsRequest != null) {
+			getPermissionsRequest.cancel();
+		}
+
+		getPermissionsRequest = ApiHelper.createUserClient().getPermissions(
+				input, new AsyncCallback<GetPermissionsResponse>() {
+
+					@Override
+					public void onSuccess (GetPermissionsResponse output) {
+						if (output.status == StatusType.StatusTypeSuccess
+								&& output.pager != null) {
+							foundItems(request, callback, output.permissions);
+						} else {
+							foundItems(request, callback,
+									Collections.<Permission> emptyList());
+						}
+					}
+
+					@Override
+					public void onFailure (Throwable caught) {
+						GWT.log("Error getting permissions with query "
+								+ input.query, caught);
+
+						foundItems(request, callback,
+								Collections.<Permission> emptyList());
+					}
+				});
 	}
 
 	/* (non-Javadoc)
@@ -56,5 +99,13 @@ public class PermissionOracle extends SuggestOracle<Permission> {
 	@Override
 	public boolean isDisplayStringHTML () {
 		return true;
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see com.willshex.blogwt.client.oracle.SuggestOracle#getQueryMinLength() */
+	@Override
+	protected int getQueryMinLength () {
+		return 0;
 	}
 }
--- src/main/java/com/willshex/blogwt/client/oracle/RoleOracle.java
+++ src/main/java/com/willshex/blogwt/client/oracle/RoleOracle.java
@@ -12,7 +12,14 @@ import java.util.Collections;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.safehtml.client.SafeHtmlTemplates;
 import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.willshex.blogwt.client.controller.SessionController;
+import com.willshex.blogwt.client.helper.ApiHelper;
 import com.willshex.blogwt.shared.api.datatype.Role;
+import com.willshex.blogwt.shared.api.user.call.GetRolesRequest;
+import com.willshex.blogwt.shared.api.user.call.GetRolesResponse;
+import com.willshex.blogwt.shared.helper.PagerHelper;
+import com.willshex.gson.json.service.shared.StatusType;
 
 /**
  * @author William Shakour (billy1380)
@@ -25,10 +32,11 @@ public class RoleOracle extends SuggestOracle<Role> {
 		NameDescriptionTemplates INSTANCE = GWT
 				.create(NameDescriptionTemplates.class);
 
-		@Template("{0}<div class=\"smaller text-muted\">{1}</div>")
+		@Template("{0}<div class=\"small text-muted\">{1}</div>")
 		SafeHtml description (String name, String description);
-
 	}
+
+	private com.google.gwt.http.client.Request getRolesRequest;
 
 	/* (non-Javadoc)
 	 * 
@@ -37,8 +45,41 @@ public class RoleOracle extends SuggestOracle<Role> {
 	 * .user.client.ui.SuggestOracle.Request,
 	 * com.google.gwt.user.client.ui.SuggestOracle.Callback) */
 	@Override
-	protected void lookup (Request request, Callback callback) {
-		this.foundItems(request, callback, Collections.<Role> emptyList());
+	protected void lookup (final Request request, final Callback callback) {
+		final GetRolesRequest input = ApiHelper
+				.setAccessCode(new GetRolesRequest());
+		input.session = SessionController.get().sessionForApiCall();
+		input.query = request.getQuery();
+		input.pager = PagerHelper.createDefaultPager();
+		input.pager.count = Integer.valueOf(request.getLimit());
+
+		if (getRolesRequest != null) {
+			getRolesRequest.cancel();
+		}
+
+		getRolesRequest = ApiHelper.createUserClient().getRoles(input,
+				new AsyncCallback<GetRolesResponse>() {
+
+					@Override
+					public void onSuccess (GetRolesResponse output) {
+						if (output.status == StatusType.StatusTypeSuccess
+								&& output.pager != null) {
+							foundItems(request, callback, output.roles);
+						} else {
+							foundItems(request, callback,
+									Collections.<Role> emptyList());
+						}
+					}
+
+					@Override
+					public void onFailure (Throwable caught) {
+						GWT.log("Error getting roles with query " + input.query,
+								caught);
+
+						foundItems(request, callback,
+								Collections.<Role> emptyList());
+					}
+				});
 	}
 
 	/* (non-Javadoc)
@@ -68,6 +109,14 @@ public class RoleOracle extends SuggestOracle<Role> {
 	@Override
 	public boolean isDisplayStringHTML () {
 		return true;
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see com.willshex.blogwt.client.oracle.SuggestOracle#getQueryMinLength() */
+	@Override
+	protected int getQueryMinLength () {
+		return 0;
 	}
 
 }
