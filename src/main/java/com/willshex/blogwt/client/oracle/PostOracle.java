5cd068342d424907c1edcf5b100a8be7a61b8396
billy1380 <william.shakour@gmail.com>
2015-10-06 12:17:31 +0100

    added api call to post oracle

--- src/main/java/com/willshex/blogwt/client/oracle/PostOracle.java
+++ src/main/java/com/willshex/blogwt/client/oracle/PostOracle.java
@@ -9,13 +9,23 @@ package com.willshex.blogwt.client.oracle;
 
 import java.util.Collections;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.willshex.blogwt.client.controller.SessionController;
+import com.willshex.blogwt.client.helper.ApiHelper;
+import com.willshex.blogwt.shared.api.blog.call.GetPostsRequest;
+import com.willshex.blogwt.shared.api.blog.call.GetPostsResponse;
 import com.willshex.blogwt.shared.api.datatype.Post;
+import com.willshex.blogwt.shared.helper.PagerHelper;
+import com.willshex.gson.json.service.shared.StatusType;
 
 /**
  * @author William Shakour (billy1380)
  *
  */
 public class PostOracle extends SuggestOracle<Post> {
+
+	private com.google.gwt.http.client.Request getPostsRequest;
 
 	/* (non-Javadoc)
 	 * 
@@ -24,8 +34,42 @@ public class PostOracle extends SuggestOracle<Post> {
 	 * .user.client.ui.SuggestOracle.Request,
 	 * com.google.gwt.user.client.ui.SuggestOracle.Callback) */
 	@Override
-	protected void lookup (Request request, Callback callback) {
-		this.foundItems(request, callback, Collections.<Post> emptyList());
+	protected void lookup (final Request request, final Callback callback) {
+		final GetPostsRequest input = ApiHelper
+				.setAccessCode(new GetPostsRequest());
+		input.session = SessionController.get().sessionForApiCall();
+		input.includePostContents = Boolean.FALSE;
+		input.query = request.getQuery();
+		input.pager = PagerHelper.createDefaultPager();
+		input.pager.count = Integer.valueOf(request.getLimit());
+
+		if (getPostsRequest != null) {
+			getPostsRequest.cancel();
+		}
+
+		getPostsRequest = ApiHelper.createBlogClient().getPosts(input,
+				new AsyncCallback<GetPostsResponse>() {
+
+					@Override
+					public void onSuccess (GetPostsResponse output) {
+						if (output.status == StatusType.StatusTypeSuccess
+								&& output.pager != null) {
+							foundItems(request, callback, output.posts);
+						} else {
+							foundItems(request, callback,
+									Collections.<Post> emptyList());
+						}
+					}
+
+					@Override
+					public void onFailure (Throwable caught) {
+						GWT.log("Error getting posts with query " + input.query,
+								caught);
+
+						foundItems(request, callback,
+								Collections.<Post> emptyList());
+					}
+				});
 	}
 
 	/* (non-Javadoc)
