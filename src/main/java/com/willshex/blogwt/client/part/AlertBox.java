897c5a6bcf5ed7d7b64f5f4d318fcc047533ef8c
billy1380 <william.shakour@gmail.com>
2015-11-27 22:03:09 +0000

    fixed alert box

--- src/main/java/com/willshex/blogwt/client/part/AlertBox.java
+++ src/main/java/com/willshex/blogwt/client/part/AlertBox.java
@@ -21,6 +21,7 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Widget;
+import com.willshex.blogwt.client.Resources;
 
 /**
  * @author William Shakour (billy1380)
@@ -53,6 +54,14 @@ public class AlertBox extends Composite implements HasCloseHandlers<AlertBox> {
 	}
 
 	public void setCanDismiss (boolean value) {
+		if (value != this.getCanDismiss()) {
+			if (value) {
+				getWidget().addStyleName("alert-dismissible");
+			} else {
+				getWidget().removeStyleName("alert-dismissible");
+			}
+		}
+
 		btnClose.setVisible(value);
 	}
 
@@ -61,7 +70,41 @@ public class AlertBox extends Composite implements HasCloseHandlers<AlertBox> {
 	}
 
 	public void setType (AlertBoxType type) {
+		switch (type) {
+		case DangerAlertBoxType:
+			getWidget().removeStyleName("alert-danger");
+			break;
+		case InfoAlertBoxType:
+			getWidget().removeStyleName("alert-info");
+			break;
+		case SuccessAlertBoxType:
+			getWidget().removeStyleName("alert-success");
+			break;
+		case WarningAlertBoxType:
+			getWidget().removeStyleName("alert-warning");
+			break;
+		}
+
 		this.type = type;
+
+		switch (type) {
+		case DangerAlertBoxType:
+			getWidget().addStyleName("alert-danger");
+			imgSpinner.setResource(Resources.RES.dangerLoader());
+			break;
+		case InfoAlertBoxType:
+			getWidget().addStyleName("alert-info");
+			imgSpinner.setResource(Resources.RES.infoLoader());
+			break;
+		case SuccessAlertBoxType:
+			getWidget().addStyleName("alert-success");
+			imgSpinner.setResource(Resources.RES.successLoader());
+			break;
+		case WarningAlertBoxType:
+			getWidget().addStyleName("alert-warning");
+			imgSpinner.setResource(Resources.RES.warningLoader());
+			break;
+		}
 	}
 
 	public AlertBoxType getType () {
@@ -70,9 +113,6 @@ public class AlertBox extends Composite implements HasCloseHandlers<AlertBox> {
 
 	public void setLoading (boolean loading) {
 		imgSpinner.setVisible(loading);
-		if (loading) {
-			setType(AlertBoxType.InfoAlertBoxType);
-		}
 	}
 
 	public boolean getLoading () {
