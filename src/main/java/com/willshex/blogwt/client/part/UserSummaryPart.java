0080dd36a9a21f929319e6af5dfa2b35e55a05e0
billy1380 <william.shakour@gmail.com>
2015-12-05 18:57:14 +0000

    updated user summary to display user details and text

--- src/main/java/com/willshex/blogwt/client/part/UserSummaryPart.java
+++ src/main/java/com/willshex/blogwt/client/part/UserSummaryPart.java
@@ -8,10 +8,15 @@
 package com.willshex.blogwt.client.part;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Widget;
+import com.willshex.blogwt.client.helper.PostHelper;
 import com.willshex.blogwt.shared.api.datatype.User;
+import com.willshex.blogwt.shared.helper.UserHelper;
 
 /**
  * @author William Shakour (billy1380)
@@ -24,6 +29,11 @@ public class UserSummaryPart extends Composite {
 
 	interface UserSummaryPartUiBinder extends UiBinder<Widget, UserSummaryPart> {}
 
+	@UiField Image imgAvatar;
+	@UiField Element elName;
+	@UiField Element elUsername;
+	@UiField Element elSummary;
+
 	public UserSummaryPart () {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
@@ -33,6 +43,18 @@ public class UserSummaryPart extends Composite {
 	}
 
 	private void show (User user) {
+		String username = "@" + user.username;
+		imgAvatar.setAltText(username);
+		imgAvatar.setUrl(user.avatar + "?s=80&default=retro");
+		elName.setInnerHTML(UserHelper.name(user));
+		elUsername.setInnerHTML(username);
 
+		if (user.summary != null) {
+			elSummary.setInnerHTML(PostHelper.makeMarkup(user.summary));
+		} else {
+			elSummary.setInnerHTML("<p class=\"text-muted text-justify\">"
+					+ UserHelper.name(user)
+					+ " has not entered a user summary.</p>");
+		}
 	}
 }
