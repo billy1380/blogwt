e3998edbcf1ccd7dede3f4a8c7a628b7978c3291
billy1380 <william.shakour@gmail.com>
2015-12-15 20:02:17 +0000

    added arrows and set slugs or hide if no slugs set

--- src/main/java/com/willshex/blogwt/client/part/PostNavPart.java
+++ src/main/java/com/willshex/blogwt/client/part/PostNavPart.java
@@ -8,9 +8,14 @@
 package com.willshex.blogwt.client.part;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.AnchorElement;
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.willshex.blogwt.client.helper.PageTypeHelper;
+import com.willshex.blogwt.shared.page.PageType;
 
 /**
  * @author William Shakour (billy1380)
@@ -23,8 +28,40 @@ public class PostNavPart extends Composite {
 
 	interface PostNavPartUiBinder extends UiBinder<Widget, PostNavPart> {}
 
+	@UiField AnchorElement btnPrevious;
+	@UiField Element elPrevious;
+
+	@UiField AnchorElement btnNext;
+	@UiField Element elNext;
+
 	public PostNavPart () {
 		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+	/**
+	 * @param previousSlug
+	 * @param nextSlug
+	 */
+	public void setSlugs (String previousSlug, String nextSlug) {
+		setVisible(nextSlug != null || previousSlug != null);
+
+		if (previousSlug == null) {
+			elPrevious.addClassName("hidden");
+			btnPrevious.setHref(PageTypeHelper.slugToHref(""));
+		} else {
+			elPrevious.removeClassName("hidden");
+			btnPrevious.setHref(PageTypeHelper.asHref(
+					PageType.PostDetailPageType, previousSlug));
+		}
+
+		if (nextSlug == null) {
+			elNext.addClassName("hidden");
+			btnNext.setHref(PageTypeHelper.slugToHref(""));
+		} else {
+			elNext.removeClassName("hidden");
+			btnNext.setHref(PageTypeHelper.asHref(PageType.PostDetailPageType,
+					nextSlug));
+		}
 	}
 
 }
