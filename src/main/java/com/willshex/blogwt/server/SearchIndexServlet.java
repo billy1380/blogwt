c869f323b42129d239427d31a792c87add7c7aac
billy1380 <william.shakour@gmail.com>
2016-01-01 20:05:01 +0000

    added index calls to servlet for parameters

--- src/main/java/com/willshex/blogwt/server/SearchIndexServlet.java
+++ src/main/java/com/willshex/blogwt/server/SearchIndexServlet.java
@@ -11,6 +11,12 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 
+import com.willshex.blogwt.server.service.page.IPageService;
+import com.willshex.blogwt.server.service.page.PageServiceProvider;
+import com.willshex.blogwt.server.service.post.IPostService;
+import com.willshex.blogwt.server.service.post.PostServiceProvider;
+import com.willshex.blogwt.server.service.user.IUserService;
+import com.willshex.blogwt.server.service.user.UserServiceProvider;
 import com.willshex.service.ContextAwareServlet;
 
 /**
@@ -28,7 +34,26 @@ public class SearchIndexServlet extends ContextAwareServlet {
 	protected void doGet () throws ServletException, IOException {
 		super.doGet();
 
-		
+		String nameParam = REQUEST.get().getParameter("name");
+		String idParam = REQUEST.get().getParameter("id");
+
+		if (nameParam != null && idParam != null) {
+			Long id = Long.valueOf(idParam);
+
+			switch (nameParam) {
+			case IUserService.NAME:
+				UserServiceProvider.provide().indexUser(id);
+				break;
+			case IPostService.NAME:
+				PostServiceProvider.provide().indexPost(id);
+				break;
+			case IPageService.NAME:
+				PageServiceProvider.provide().indexPage(id);
+				break;
+			default:
+				break;
+			}
+		}
 	}
 
 	/* (non-Javadoc)
