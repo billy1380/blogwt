e72b838ee15c9a2600bbee682af9592a5a186ec1
billy1380 <william.shakour@gmail.com>
2016-02-05 13:34:03 +0000

    implemented relationship service methods

--- src/main/java/com/willshex/blogwt/server/service/relationship/RelationshipService.java
+++ src/main/java/com/willshex/blogwt/server/service/relationship/RelationshipService.java
@@ -1,12 +1,17 @@
 //  
 //  RelationshipService.java
-//  xsdwsdl2code
+//  blogwt
 //
 //  Created by William Shakour on February 5, 2016.
 //  Copyright © 2016 WillShex Limited. All rights reserved.
 //
 package com.willshex.blogwt.server.service.relationship;
 
+import static com.willshex.blogwt.server.service.PersistenceService.ofy;
+
+import java.util.Date;
+
+import com.googlecode.objectify.Key;
 import com.willshex.blogwt.shared.api.datatype.Relationship;
 
 final class RelationshipService implements IRelationshipService {
@@ -16,22 +21,46 @@ final class RelationshipService implements IRelationshipService {
 
 	@Override
 	public Relationship getRelationship (Long id) {
-		throw new UnsupportedOperationException();
+		return ofy().load().type(Relationship.class).id(id.longValue()).now();
 	}
 
 	@Override
 	public Relationship addRelationship (Relationship relationship) {
-		throw new UnsupportedOperationException();
+		if (relationship.created == null) {
+			relationship.created = new Date();
+		}
+
+		if (relationship.one != null) {
+			relationship.oneKey = Key.create(relationship.one);
+		}
+
+		if (relationship.another != null) {
+			relationship.anotherKey = Key.create(relationship.another);
+		}
+
+		Key<Relationship> key = ofy().save().entity(relationship).now();
+		relationship.id = Long.valueOf(key.getId());
+		return relationship;
 	}
 
 	@Override
 	public Relationship updateRelationship (Relationship relationship) {
-		throw new UnsupportedOperationException();
+		if (relationship.one != null) {
+			relationship.oneKey = Key.create(relationship.one);
+		}
+
+		if (relationship.another != null) {
+			relationship.anotherKey = Key.create(relationship.another);
+		}
+
+		ofy().save().entity(relationship).now();
+
+		return relationship;
 	}
 
 	@Override
 	public void deleteRelationship (Relationship relationship) {
-		throw new UnsupportedOperationException();
+		ofy().delete().entity(relationship).now();
 	}
 
 }
