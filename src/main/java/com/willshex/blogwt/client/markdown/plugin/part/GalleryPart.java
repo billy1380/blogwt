6e3260cca134086465e8a3e35ddd3770d64b7830
billy1380 <william.shakour@gmail.com>
2016-02-22 12:03:18 +0000

    added config lines parsing for image urls

--- src/main/java/com/willshex/blogwt/client/markdown/plugin/part/GalleryPart.java
+++ src/main/java/com/willshex/blogwt/client/markdown/plugin/part/GalleryPart.java
@@ -7,11 +7,14 @@
 //
 package com.willshex.blogwt.client.markdown.plugin.part;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
@@ -24,6 +27,13 @@ public class GalleryPart extends Composite {
 			.create(GalleryPartUiBinder.class);
 
 	interface GalleryPartUiBinder extends UiBinder<Widget, GalleryPart> {}
+
+	private static class ConfigLine {
+		public String url;
+		public Map<String, String> parameters;
+	}
+
+	private static final String URL = "url";
 
 	public GalleryPart () {
 		initWidget(uiBinder.createAndBindUi(this));
@@ -40,7 +50,39 @@ public class GalleryPart extends Composite {
 	 * @param line
 	 */
 	public void addImageWithLine (String line) {
+		if (line.length() > 0) {
+			ConfigLine config = parseConfigLine(line);
 
+			if (config.url != null && config.url.length() > 0) {
+				Image image = new Image(config.url);
+
+				((HTMLPanel) this.getWidget()).add(image);
+			}
+		}
 	}
 
+	private ConfigLine parseConfigLine (String line) {
+		String[] params = line.split("/");
+		ConfigLine config = new ConfigLine();
+
+		String[] splitParam;
+		Map<String, String> parameters = new HashMap<String, String>();
+		for (String param : params) {
+			splitParam = param.split("=");
+
+			if (splitParam.length == 2) {
+				parameters.put(splitParam[0], splitParam[1]);
+
+				switch (splitParam[0]) {
+				case URL:
+					config.url = splitParam[1];
+					break;
+				}
+			}
+		}
+
+		config.parameters = parameters;
+
+		return config;
+	}
 }
