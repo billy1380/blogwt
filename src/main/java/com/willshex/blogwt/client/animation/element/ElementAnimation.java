5976e457a263f5d7258919e5caf468247ef5912f
billy1380 <william.shakour@gmail.com>
2017-07-19 15:42:35 +0100

    removed gquery dependency

---
+++ src/main/java/com/willshex/blogwt/client/animation/element/ElementAnimation.java
@@ -0,0 +1,47 @@
+//
+//  ElementAnimation.java
+//  blogwt
+//
+//  Created by William Shakour (billy1380) on 19 Jul 2017.
+//  Copyright © 2017 WillShex Limited. All rights reserved.
+//
+package com.willshex.blogwt.client.animation.element;
+
+import com.google.gwt.animation.client.Animation;
+import com.google.gwt.dom.client.Element;
+
+/**
+ * @author William Shakour (billy1380)
+ *
+ */
+public abstract class ElementAnimation extends Animation {
+	@FunctionalInterface
+	public interface Complete {
+		void on (Element e);
+	}
+
+	public Element e;
+	public Complete c;
+
+	/* (non-Javadoc)
+	 * 
+	 * @see com.google.gwt.animation.client.Animation#onComplete() */
+	@Override
+	protected void onComplete () {
+		super.onComplete();
+
+		if (c != null) {
+			c.on(e);
+		}
+	}
+
+	public ElementAnimation e (Element value) {
+		e = value;
+		return this;
+	}
+
+	public ElementAnimation c (Complete value) {
+		c = value;
+		return this;
+	}
+}
