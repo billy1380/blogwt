dbb308814d3ab573993866673f3aa4bff6ac9764
billy1380 <william.shakour@gmail.com>
2017-09-08 20:35:01 +0100

    can now embed markdown in html

--- src/main/java/com/willshex/blogwt/client/markdown/Processor.java
+++ src/main/java/com/willshex/blogwt/client/markdown/Processor.java
@@ -9,6 +9,9 @@ package com.willshex.blogwt.client.markdown;
 
 import static com.google.gwt.user.client.Window.Location.getHost;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.markdown4j.ExtDecorator;
 import org.markdown4j.client.MarkdownProcessor;
 import org.markdown4j.client.WebSequencePlugin;
@@ -17,6 +20,7 @@ import com.github.rjeschke.txtmark.Decorator;
 import com.github.rjeschke.txtmark.EmojiEmitter;
 import com.github.rjeschke.txtmark.MarkdownUtils;
 import com.google.gwt.safehtml.shared.SafeUri;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.willshex.blogwt.client.Resources;
 import com.willshex.blogwt.client.controller.PropertyController;
 import com.willshex.blogwt.client.markdown.plugin.CachedIncludePlugin;
@@ -43,6 +47,10 @@ public class Processor extends MarkdownProcessor {
 	private static final String HOST_START = ANY + getHost();
 	private static final String HTTP_HOST_START = HTTP + HOST_START;
 	private static final String HTTPS_HOST_START = HTTPS + HOST_START;
+
+	private static final String OPEN = "<md>", CLOSE = "</md>";
+	private static final int OPEN_LENGTH = OPEN.length(),
+			CLOSE_LENGTH = CLOSE.length();
 
 	/* (non-Javadoc)
 	 * 
@@ -162,6 +170,60 @@ public class Processor extends MarkdownProcessor {
 			}
 		});
 		decorator.addStyleClass("text-justify", "p");
+	}
+
+	/* (non-Javadoc)
+	 * 
+	 * @see org.markdown4j.client.MarkdownProcessor#process(java.lang.String) */
+	@Override
+	public String process (String markdown) {
+		StringBuffer b = new StringBuffer(markdown);
+
+		Map<String, String> replaced = new HashMap<>();
+
+		String id;
+
+		int start = 0, end;
+		while ((start = b.indexOf(OPEN, start)) > 0) {
+			if ((end = b.indexOf(CLOSE, start)) > 0) {
+				id = HTMLPanel.createUniqueId();
+				replaced.put(id, b.substring(start + OPEN_LENGTH, end));
+				b.replace(start, end + CLOSE_LENGTH, marker(id));
+			}
+		}
+
+		String processed = super.process(b.toString());
+
+		if (replaced.size() > 0) {
+			b.setLength(0);
+			b.append(processed);
+
+			String match, content;
+			int matchLength;
+			start = 0;
+			for (String key : replaced.keySet()) {
+				match = marker(key);
+				matchLength = match.length();
+
+				while ((start = b.indexOf(match, start)) > 0) {
+					b.replace(start, start + matchLength,
+							content = process(replaced.get(key)));
+					start += content.length();
+				}
+			}
+
+			processed = b.toString();
+		}
+
+		return processed;
+	}
+
+	/**
+	 * @param id
+	 * @return
+	 */
+	private String marker (String id) {
+		return "<span id=\"md:" + id + "\">" + id + "</span>";
 	}
 
 	/**
