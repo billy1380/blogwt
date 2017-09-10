30322ab7198050f081c808f2c5c41e372637c30e
billy1380 <william.shakour@gmail.com>
2017-09-11 00:27:49 +0100

    do not open new page if internal copyright link

--- src/main/java/com/willshex/blogwt/client/part/FooterPart.java
+++ src/main/java/com/willshex/blogwt/client/part/FooterPart.java
@@ -18,7 +18,9 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.willshex.blogwt.client.Resources;
 import com.willshex.blogwt.client.controller.PropertyController;
+import com.willshex.blogwt.client.helper.ApiHelper;
 
 public class FooterPart extends Composite {
 
@@ -30,9 +32,13 @@ public class FooterPart extends Composite {
 	public interface FooterTemplates extends SafeHtmlTemplates {
 		FooterTemplates FOOTER_TEMPLATES = GWT.create(FooterTemplates.class);
 
-		@Template("Copyright &copy; <a href=\"{0}\" target=\"_blank\" rel=\"noopener\">{1}</a> {2}. All rights reserved - {3}.")
+		@Template("Copyright &copy; <a href=\"{0}\" class=\"{4}\" target=\"_blank\" rel=\"noopener\">{1}</a> {2}. All rights reserved - {3}.")
 		SafeHtml copyrightNotice (SafeUri uri, String holder, String years,
-				String name);
+				String name, String styleNames);
+
+		@Template("Copyright &copy; <a href=\"{0}\">{1}</a> {2}. All rights reserved - {3}.")
+		SafeHtml copyrightNoticeInternal (SafeUri uri, String holder,
+				String years, String name);
 	}
 
 	@UiField DivElement divCopyright;
@@ -43,10 +49,19 @@ public class FooterPart extends Composite {
 
 		PropertyController controller = PropertyController.get();
 
-		divCopyright.setInnerSafeHtml(FooterTemplates.FOOTER_TEMPLATES
-				.copyrightNotice(controller.copyrightHolderUrl(),
-						controller.copyrightHolder(), years(controller),
-						controller.title()));
+		SafeUri url = controller.copyrightHolderUrl();
+		String urlAsString = url.asString();
+		if (urlAsString.contains(ApiHelper.HOST)
+				|| urlAsString.startsWith("#")) {
+			divCopyright.setInnerSafeHtml(FooterTemplates.FOOTER_TEMPLATES
+					.copyrightNoticeInternal(url, controller.copyrightHolder(),
+							years(controller), controller.title()));
+		} else {
+			divCopyright.setInnerSafeHtml(FooterTemplates.FOOTER_TEMPLATES
+					.copyrightNotice(url, controller.copyrightHolder(),
+							years(controller), controller.title(),
+							Resources.RES.styles().externalLink()));
+		}
 	}
 
 	@SuppressWarnings("deprecation")
