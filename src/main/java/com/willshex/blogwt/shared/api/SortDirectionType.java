782622476896d2e53bfe4154ef55fc725af9bc49
billy1380 <william.shakour@gmail.com>
2018-06-10 10:48:37 +0100

    sort direction can be reversed

--- src/main/java/com/willshex/blogwt/shared/api/SortDirectionType.java
+++ src/main/java/com/willshex/blogwt/shared/api/SortDirectionType.java
@@ -12,25 +12,32 @@ import java.util.Map;
 
 public enum SortDirectionType {
 	SortDirectionTypeAscending("ascending"),
-	SortDirectionTypeDescending("descending"), ;
+	SortDirectionTypeDescending("descending"),;
 	private String value;
 	private static Map<String, SortDirectionType> valueLookup = null;
 
-	public String toString() {
+	public String toString () {
 		return value;
 	}
 
-	private SortDirectionType(String value) {
+	private SortDirectionType (String value) {
 		this.value = value;
 	}
 
-	public static SortDirectionType fromString(String value) {
+	public static SortDirectionType fromString (String value) {
 		if (valueLookup == null) {
 			valueLookup = new HashMap<String, SortDirectionType>();
-			for (SortDirectionType currentSortDirectionType : SortDirectionType.values()) {
-				valueLookup.put(currentSortDirectionType.value, currentSortDirectionType);
+			for (SortDirectionType currentSortDirectionType : SortDirectionType
+					.values()) {
+				valueLookup.put(currentSortDirectionType.value,
+						currentSortDirectionType);
 			}
 		}
 		return valueLookup.get(value);
+	}
+
+	public SortDirectionType reverse () {
+		return this == SortDirectionTypeAscending ? SortDirectionTypeDescending
+				: SortDirectionTypeAscending;
 	}
 }
