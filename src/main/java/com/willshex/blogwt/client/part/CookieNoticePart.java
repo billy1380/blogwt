1040e962020831b242f7de49c81c5ba867d9a045
billy1380 <william.shakour@gmail.com>
2018-06-10 11:06:36 +0100

    cookie notice tidy up

--- src/main/java/com/willshex/blogwt/client/part/CookieNoticePart.java
+++ src/main/java/com/willshex/blogwt/client/part/CookieNoticePart.java
@@ -10,7 +10,6 @@ package com.willshex.blogwt.client.part;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
@@ -33,10 +32,6 @@ public class CookieNoticePart extends RegisteringComposite {
 
 	interface CookieNoticePartUiBinder
 			extends UiBinder<Widget, CookieNoticePart> {}
-
-	interface Style extends CssResource {
-		String fixed ();
-	}
 
 	private static final String EU_COOKIE_NOTICE_KEY = "eu.cookie.notice";
 
