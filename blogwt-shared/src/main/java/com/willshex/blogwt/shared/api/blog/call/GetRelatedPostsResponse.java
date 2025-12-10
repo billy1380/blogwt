87eb20d2d2fbbd5e72793a917a0d35bfeac74f62
William Shakour (billy1380) <william.shakour@gmail.com>
2025-12-10 13:41:31 +0000

    Add List, ArrayList, and JsonArray imports to GetRelatedPostsResponse.

--- blogwt-shared/src/main/java/com/willshex/blogwt/shared/api/blog/call/GetRelatedPostsResponse.java
+++ blogwt-shared/src/main/java/com/willshex/blogwt/shared/api/blog/call/GetRelatedPostsResponse.java
@@ -1,12 +1,17 @@
-//  
+//
 //  GetRelatedPostsResponse.java
 //  blogwt
 //
-//  Created by William Shakour on August 7, 2015.
-//  Copyright © 2015 WillShex Limited. All rights reserved.
+//  Created by William Shakour on December 10, 2025.
+//  Copyright © 2025 WillShex Limited. All rights reserved.
 //
+
 package com.willshex.blogwt.shared.api.blog.call;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
@@ -15,31 +20,45 @@ import com.willshex.blogwt.shared.api.Response;
 import com.willshex.blogwt.shared.api.datatype.Post;
 
 public class GetRelatedPostsResponse extends Response {
-	public Post posts;
+	public List<Post> posts;
 	public Pager pager;
 
 	@Override
-	public JsonObject toJson () {
+	public JsonObject toJson() {
 		JsonObject object = super.toJson();
-		JsonElement jsonPosts = posts == null ? JsonNull.INSTANCE : posts
-				.toJson();
+		JsonElement jsonPosts = JsonNull.INSTANCE;
+		if (posts != null) {
+			jsonPosts = new JsonArray();
+			for (int i = 0; i < posts.size(); i++) {
+				JsonElement jsonPostsItem = posts.get(i) == null ? JsonNull.INSTANCE
+						: posts.get(i).toJson();
+				((JsonArray) jsonPosts).add(jsonPostsItem);
+			}
+		}
 		object.add("posts", jsonPosts);
-		JsonElement jsonPager = pager == null ? JsonNull.INSTANCE : pager
-				.toJson();
+		JsonElement jsonPager = pager == null ? JsonNull.INSTANCE : pager.toJson();
 		object.add("pager", jsonPager);
 		return object;
 	}
 
 	@Override
-	public void fromJson (JsonObject jsonObject) {
+	public void fromJson(JsonObject jsonObject) {
 		super.fromJson(jsonObject);
 		if (jsonObject.has("posts")) {
 			JsonElement jsonPosts = jsonObject.get("posts");
 			if (jsonPosts != null) {
-				posts = new Post();
-				posts.fromJson(jsonPosts.getAsJsonObject());
+				posts = new ArrayList<Post>();
+				Post item = null;
+				for (int i = 0; i < jsonPosts.getAsJsonArray().size(); i++) {
+					if (jsonPosts.getAsJsonArray().get(i) != null) {
+						(item = new Post())
+								.fromJson(jsonPosts.getAsJsonArray().get(i).getAsJsonObject());
+						posts.add(item);
+					}
+				}
 			}
 		}
+
 		if (jsonObject.has("pager")) {
 			JsonElement jsonPager = jsonObject.get("pager");
 			if (jsonPager != null) {
@@ -49,12 +68,24 @@ public class GetRelatedPostsResponse extends Response {
 		}
 	}
 
-	public GetRelatedPostsResponse posts (Post posts) {
+	/**
+	 * Fluent setter for posts.
+	 * 
+	 * @param posts
+	 * @return
+	 */
+	public GetRelatedPostsResponse posts(List<Post> posts) {
 		this.posts = posts;
 		return this;
 	}
 
-	public GetRelatedPostsResponse pager (Pager pager) {
+	/**
+	 * Fluent setter for pager.
+	 * 
+	 * @param pager
+	 * @return
+	 */
+	public GetRelatedPostsResponse pager(Pager pager) {
 		this.pager = pager;
 		return this;
 	}
